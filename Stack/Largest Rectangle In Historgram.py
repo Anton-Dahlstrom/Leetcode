593c77168279611f3050c039f129cd8827f1eecb
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-01-12 23:58:09 +0100

    Giving wrong answers with zeroes in middle of input

--- Stack/Largest Rectangle In Historgram.py
+++ Stack/Largest Rectangle In Historgram.py
@@ -1,7 +1,10 @@
 heights = [2,1,5,6,2,3]
 heights = [2,4]
 heights =[0,9]
-# output = 10
+heights = [2,0,2]
+
+heights =[4,2,0,3,2,4,3,4]
+# Answer: 10
 
 # Move the pointer on the lowest height and calculate the largest possible 
 # rectangle they can make each turn.
@@ -18,21 +21,38 @@ class Solution:
             print(heights[l], heights[r])
             length = r - l + 1
             multiplier = min(heights[l], heights[r])
+            if not multiplier:
+                multiplier = max(heights[l], heights[r])
+                length -= 1
+                highest = []
             temp = [multiplier * length, length, multiplier]
             print(multiplier, length)
             print(temp)
+            print("L, R", l, r)
             if highest:
                 if multiplier < highest[2]:
                     highest[2] = multiplier
                     highest[0] = highest[1]*highest[2]
                 if temp[0] > highest[0]:
                     highest = temp
-                if heights[r] > heights[l]:
-                    l+=1
-                else:
-                    r-=1
             else:
                 highest = temp
+            if heights[r] == heights[l]:
+                steps = 0
+                while r-steps > l+steps:
+                    steps += 1
+                    if heights[r-steps] > heights[l+steps]:
+                        r-=1
+                        break
+                    elif heights[r-steps] < heights[l+steps]:
+                        l+=1
+                        break
+                    else:
+                        steps += 1
+            elif heights[r] > heights[l]:
+                l+=1
+            else:
+                r-=1
         if highest:
             return highest[0]
         else:
