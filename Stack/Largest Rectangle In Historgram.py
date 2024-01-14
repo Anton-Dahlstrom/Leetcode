d5414de73f0992d54f48edd3535ada66d94db075
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-01-14 18:29:00 +0100

    Solution

--- Stack/Largest Rectangle In Historgram.py
+++ Stack/Largest Rectangle In Historgram.py
@@ -1,43 +1,27 @@
-heights = [2,1,5,6,2,3]
-heights = [2,4]
-heights =[0,9]
-heights = [2,0,2]
-
 heights =[4,2,0,3,2,4,3,4]
 # Answer: 10
 
-# Move the pointer on the lowest height and calculate the largest possible 
-# rectangle they can make each turn.
-# Need to save what the lowest column have been since it's the height multiplier.
-# If a pointer runs into a lower height the height of the lowest column for that
-# rectangle has to change.
-
 class Solution:
     def largestRectangleArea(self, heights: list[int]) -> int:
-        stack = [] #height, index, open
+        stack = [] #height, index
         largest = 0
-        toPop = []
         for i, value in enumerate(heights):
             stack.append([value, i])
-            for j, rectangle in enumerate(stack):
-                if value < rectangle[0]:
-                    size = (rectangle[0] * i + 1 - rectangle[1])
-                    if size > largest:
-                        print(largest)
-                        largest = size
-                    toPop.append(j)
-
-            if toPop:
-                for index in toPop:
-                    print(stack, index)
-                    stack.pop(index)
-                toPop = []
-
-        for rectangle in stack:
-            print(largest)
-            size = (rectangle[0] * i + 1 - rectangle[1])
-            if size > largest:
-                largest = size
+            while len(stack) > 1:
+                top = stack[-1]
+                if top[0] < stack[-2][0]:
+                    under = stack.pop(-2)
+                    size = under[0] * (i - under[1])
+                    print(largest)
+                    largest = max(largest, size)
+                    top[1] = under[1]
+                    continue
+                else:
+                    break
+        while stack:
+            rect = stack.pop(-1)
+            size = rect[0] * (i + 1 - rect[1])
+            largest = max(largest, size)
         return largest
     
 obj = Solution()
