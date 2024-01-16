e0ae723be4e83d2d31eb57f24c713d5e5185164b
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-01-16 23:12:19 +0100

    Working on new approach

--- Sliding Window/Longest Repeating Character Replacement.py
+++ Sliding Window/Longest Repeating Character Replacement.py
@@ -13,53 +13,29 @@ k = 2
 # 4
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        letter = ""
-        skips = k
-        counter = 0
+        letters = {} # Index, count
+        capacity = k
+        overCapacity = False
         longest = 0
-        nextIndex = 0
         i = 0
+        # Maybe just keep track of the largest?
         while i < len(s):
             char = s[i]
-            if not letter:
-                letter = char
-                counter += 1
-                i += 1
-                continue
-            if char != letter:
-                if skips == k:
-                    nextIndex = i
-                if skips:
-                    skips -= 1
-                    counter += 1
-                else:
-                    if nextIndex:
-                        i = nextIndex-1
-                        lastIndex = nextIndex
-                        nextIndex = 0
-                    letter = ""
-                    longest = max(counter, longest)
-                    counter = 0
+            if letters.get(char):
+                letters[char] += 1
+                if letters[char] > k:
+                    if not overCapacity:
+                        overCapacity = True
+                    else:
+                        pass # move i to the next index
+                        overCapacity = False
             else:
-                counter += 1
-                longest = max(counter, longest)
-            i += 1
+                letters[char] = 1
 
-        if k:
-            print(lastIndex, "HERE")
-            print(counter)
-            print("hi")
-            for char in s[lastIndex::-1]:
-                print(counter)
-                counter += 1
-                if char != letter:
-                    k -= 1
-                    if k == 0:
-                        break
-        longest = max(counter, longest)
-        return longest
-        
+
 obj = Solution()
 answer = obj.characterReplacement(s, k)
 print(answer)
 
+asd = {"a": 4, "d": 8}
+print(asd[max(asd)])
