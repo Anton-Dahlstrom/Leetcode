b0e7bf2765f8c65529e612febbd092d3e6d77886
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-01-17 23:35:18 +0100

    Fail solution with dict keeping index and letter count

--- Sliding Window/Longest Repeating Character Replacement.py
+++ Sliding Window/Longest Repeating Character Replacement.py
@@ -1,41 +1,56 @@
 s = "ABAB" 
 k = 2
 
-# s = "AABABBA" 
-# k = 1
+s = "AABABBA" 
+k = 1
 
 # s = "BAAA"
 # k = 0
 # answer: 3
 
-s = "aABBB"
-k = 2
+# s = "aBBB"
+# k = 2
 # 4
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         letters = {} # Index, count
-        capacity = k
         overCapacity = False
         longest = 0
+        current = 0
         i = 0
+        mainLetter = ""
         # Maybe just keep track of the largest?
         while i < len(s):
+            print(i)
             char = s[i]
             if letters.get(char):
-                letters[char] += 1
-                if letters[char] > k:
+                letters[char][1] += 1
+                if letters[char][1] > k:
                     if not overCapacity:
+                        mainLetter = char
                         overCapacity = True
                     else:
-                        pass # move i to the next index
+                        del letters[mainLetter]
+                        mainLetter = ""
+                        i = letters[min(letters, key=letters.get)][0]
+                        letters = {}
                         overCapacity = False
+                        longest = max(current, longest)
+                        current = 0
+                        print(letters)
+                        continue
             else:
-                letters[char] = 1
+                letters[char] = [i, 1]
+            current += 1
+            i += 1
+        longest = max(longest, current)
+        return longest
 
 
 obj = Solution()
 answer = obj.characterReplacement(s, k)
 print(answer)
 
-asd = {"a": 4, "d": 8}
-print(asd[max(asd)])
+# asd = {"a": [2, 5], "d": [1, 500]}
+# asd2 = max(asd, key=asd.get)
+# print(asd2)
