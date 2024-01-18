1b67bab0ffdb5f57dce1f26aea8f3e094f3d412d
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-01-18 18:29:16 +0100

    Working solution

--- Sliding Window/Longest Repeating Character Replacement.py
+++ Sliding Window/Longest Repeating Character Replacement.py
@@ -1,6 +1,6 @@
 # s = "ABAB" 
 # k = 2
-#4 
+# 4 
 
 # s = "AABABBA" 
 # k = 1
@@ -10,41 +10,33 @@
 # k = 0
 # answer: 3
 
-# s = "aBBB"
-# k = 2
-# 4
+s = "aBBB"
+k = 2
+4
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         longest = 0
-        i = 0
-        while i < len(s):
-            hmap = {}
-            substring = ""
-            current = 0
-            r = i
-            i += 1
-            while r < len(s):
-                char = s[r]
-                substring += char
-                if char not in hmap:
-                    hmap[char] = 1
-                else:
-                    hmap[char] += 1
-                
-                if len(substring) - hmap[max(hmap, key=hmap.get)] > k:
-                    print(hmap, k)
-                    print(substring, hmap[max(hmap, key=hmap.get)])
-                    longest = max(longest, current)
-                    break
-                r += 1
-                current += 1
-                longest = max(current, longest)
+        l = 0
+        r = 0
+        hmap = {}
+        current = 0
+        while r < len(s):
+            char = s[r]
+            current += 1
+            if char not in hmap:
+                hmap[char] = 1
+            else:
+                hmap[char] += 1
+            if current - hmap[max(hmap, key=hmap.get)] > k:
+                while l < r and current - hmap[max(hmap, key=hmap.get)] > k:
+                    reduce = s[l]
+                    hmap[reduce] -= 1
+                    current -= 1
+                    l += 1
+            r += 1
+            longest = max(current, longest)
         return longest
 
 obj = Solution()
 answer = obj.characterReplacement(s, k)
 print(answer)
-
-# asd = {"a": [2, 5], "d": [3, 500]}
-# asd2 = max(asd, key=asd.get)
-# print(asd2)
