40d847b1c38d38eb2b175fe7a1ab411edceda8f3
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-01-24 19:53:39 +0100

    solution

--- sliding_window/567_permutation_in_string.py
+++ sliding_window/567_permutation_in_string.py
@@ -2,6 +2,13 @@ s1 = "ab"
 s2 = "eidbaooo"
 Output: True
 
+Input: s1 = "ab"
+s2 = "eidboaoo"
+Output: False
+
+s1 = "abc"
+s2 ="cccccbabbbaaaa"
+True
 
 # Turn perm string into hmap. If char not in hmap reset the hmap and start after the char.
 # If the char doesn't fit in the hmap go from the beginning of the hmap and add to the hmap count
@@ -12,22 +19,33 @@ class Solution:
         hmap = {}
         for char in s1:
            hmap[char] = 1 + hmap.get(char, 0)
-        print(hmap)
         temp = hmap.copy()
-        start = 0
-        for i, char in enumerate(s2):
+        l = 0
+        for r, char in enumerate(s2):
             if char not in hmap:
+                if max(temp.values()) < 1:
+                    return True
                 temp = hmap.copy()
-                start = 0
+                l = r+1
             else:
-                if not start:
-                    start = i
-                hmap[char] -= 1
-                if hmap[char] < 0:
-                    pass
-
-        temp[55] = 1
-        print(hmap)
+                temp[char] -= 1
+                if max(temp.values()) < 1:
+                    return True
+                if temp[char] < 0:
+                    for removingChar in s2[l:]: 
+                        print(l, r)
+                        print(removingChar)
+                        if removingChar in temp:
+                            temp[removingChar] += 1
+                        l += 1
+                        if removingChar == char:
+                            print(temp)
+                            print("breaking")
+                            break
+        if max(temp.values()) < 1:
+            return True
+        else: 
+            return False
 
 obj = Solution()
 answer = obj.checkInclusion(s1, s2)
