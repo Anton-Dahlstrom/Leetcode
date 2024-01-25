473119d6632bfe588237a917b9cae56d1b0f3a4d
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-01-25 22:22:31 +0100

    Setup problem

---
+++ binary_search/875_koko_eating_bananas.py
@@ -0,0 +1,13 @@
+piles = [3,6,7,11]
+h = 8
+Output: 4
+
+
+class Solution:
+    def minEatingSpeed(self, piles: list[int], h: int) -> int:
+        pass
+
+
+obj = Solution()
+answer = obj.search(piles, h)
+print(answer)
