447f543f965b81efb27a151e5da6d9a69be6b5a9
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-01-26 20:03:12 +0100

    pseudocode

--- binary_search/875_koko_eating_bananas.py
+++ binary_search/875_koko_eating_bananas.py
@@ -2,6 +2,12 @@ piles = [3,6,7,11]
 h = 8
 Output: 4
 
+# Sort the array
+# Look at the hours minus the length of the array. If they're the same return the biggest pile
+# Look at the pile hours minus array from the end of the array.
+# If the last pile is less than that pile times two we return the size of the pile we found.
+# If not we move each pointer in one step and make sure the piles to the right of the 
+# right pointer are less than the left pointers pile times 3.
 
 class Solution:
     def minEatingSpeed(self, piles: list[int], h: int) -> int:
