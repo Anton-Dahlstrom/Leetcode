f222c6815d6da4ed46b4a83fb23f7a319cf0967b
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-02-02 00:12:30 +0100

    Solution

--- binary_search/153_find_minimum_in_rotated_array.py
+++ binary_search/153_find_minimum_in_rotated_array.py
@@ -1,37 +1,34 @@
 nums = [3, 4, 5, 1, 2]
 Output: 1
 
-nums = [11, 13, 15, 17]
-Output: 11
+# nums = [11, 13, 15, 17]
+# Output: 11
 
-nums = [1]
-Output: 0
+# nums = [1]
+# Output: 1
 
-nums = [2, 3, 1]
-Output: 1
+# nums = [2, 3, 1]
+# Output: 1
+
+# nums = [3, 1, 2]
+# Output: 1
 
 
 class Solution:
     def findMin(self, nums: list[int]) -> int:
-        if len(nums) < 3:
-            return min(nums)
         l = 0
         r = len(nums) - 1
         while l <= r:
             mid = (l + r) // 2
-            print(l, r, mid)
-            if nums[mid] < nums[mid+1] and nums[mid] < nums[mid-1]:
-                return nums[mid]
-            # if nums[l] < nums[mid] and nums[l] < nums[r]:
-            #     return nums[l]
+            if nums[l] < nums[mid] and nums[l] < nums[r]:
+                return nums[l]
             if nums[mid] > nums[r]:
                 l = mid + 1
             elif nums[mid] < nums[r]:
-                r = mid - 1
-        # if mid:
+                r = mid
+            else:
+                return nums[l]
         return nums[mid]
-        print("hi")
-        return min(nums)
 
 
 obj = Solution()
