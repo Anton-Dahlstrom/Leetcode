fcfc7047c2eb02d72d8fe8aa5db844b2d92330e2
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-02-07 23:07:17 +0100

    Solution

--- linked_list/206_reverse_linked_list.py
+++ linked_list/206_reverse_linked_list.py
@@ -16,9 +16,27 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def reverse(self, array):
+        for value in array:
+            if self.head:
+                self.head = ListNode(value, self.head)
+            else:
+                self.head = ListNode(value)
+
     def insert_at_beginning(self, data):
         node = ListNode(data, self.head)
         self.head = node
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = ListNode(data, None)
+            return
+
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        itr.next = ListNode(data, None)
 
     def print(self):
         if self.head is None:
@@ -36,15 +54,22 @@ class LinkedList:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        head.reverse()
-        return head
+        if head:
+            prev = head
+            head = prev.next
+            prev.next = None
+            while head:
+                # print(prev.val)
+                next = head.next
+                head.next = prev
+                prev = head
+                head = next
+            return prev.val
 
 
 ll = LinkedList()
-ll.insert_at_beginning(5)
-ll.insert_at_beginning(25)
-ll.print()
-
-# obj = Solution()
-# result = obj.reverseList(head)
-# print(result)
+for i in head:
+    ll.insert_at_end(i)
+obj = Solution()
+result = obj.reverseList(ll.head)
+print(result)
