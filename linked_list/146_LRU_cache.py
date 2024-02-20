95ba2370de4641da53cabf7c513de09300b8e314
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-02-20 23:49:26 +0100

    working on solution

--- linked_list/146_LRU_cache.py
+++ linked_list/146_LRU_cache.py
@@ -2,6 +2,19 @@ text = ["LRUCache", "put", "put", "get",
         "put", "get", "put", "get", "get", "get"]
 nums = [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
 
+text = ["LRUCache",
+        "put", "put", "put",
+        "put", "get", "get",
+        "get", "get", "put",
+        "get", "get", "get",
+        "get", "get"]
+nums = [[3],
+        [1, 1], [2, 2], [3, 3],
+        [4, 4], [4], [3],
+        [2], [1], [5, 5],
+        [1], [2], [3],
+        [4], [5]]
+
 
 class LinkNode:
     def __init__(self, key=None, val=None, next=None, prev=None):
@@ -37,6 +50,7 @@ class LinkList:
             while cur:
                 print(cur.val)
                 cur = cur.next
+            print("end of print")
         self.head.prev = node
         node.next = self.head
         self.head = node
@@ -44,6 +58,7 @@ class LinkList:
     def popNode(self, node: LinkNode):
         if node == None:
             print("node is none")
+            return
         if node == self.head:
             self.head = node.next
         if node == self.tail:
@@ -71,6 +86,12 @@ class LRUCache:
         self.hmap = {}
 
     def get(self, key: int) -> int:
+        if self.linklist.head:
+            print("linklist vals get")
+            print(key)
+            print(self.linklist.head.key)
+            print(self.linklist.tail.key)
+            print("stopping")
         if key in self.hmap:
             node = self.hmap[key]
             self.linklist.popNode(node)
@@ -79,6 +100,12 @@ class LRUCache:
         return -1
 
     def put(self, key: int, value: int) -> None:
+        if self.linklist.head:
+            print(key, value)
+            print("linklist vals put")
+            print(self.linklist.head.key)
+            print(self.linklist.tail.key)
+            print("stopping")
         if key in self.hmap:
             node = self.hmap[key]
             node.val = value
@@ -99,19 +126,23 @@ class LRUCache:
                 node = LinkNode(key, value)
                 self.linklist.insertBeginning(node)
                 self.hmap[key] = node
-                self.linklist.print()
+                # self.linklist.print()
 
 
-ll = LinkList()
-node1 = LinkNode(1, 5)
-node2 = LinkNode(2, 6)
-node3 = LinkNode(3, 7)
-ll.insertBeginning(node1)
-ll.insertBeginning(node2)
-ll.popNode(node2)
-ll.insertBeginning(node3)
-ll.insertBeginning(node2)
-ll.print()
+cache = LRUCache(3)
+for i in range(1, len(nums)):
+    if text[i] == "get":
+        cache.get(nums[i][0])
+    elif text[i] == "put":
+        cache.put(nums[i][0], nums[i][1])
 
-
-# lRUCache = LRUCache(nums[0])
+# lRUCache = LRUCache(2)
+# lRUCache.put(1, 1)
+# lRUCache.put(2, 2)
+# print(lRUCache.get(1))
+# lRUCache.put(3, 3)
+# print(lRUCache.get(2))
+# lRUCache.put(4, 4)
+# print(lRUCache.get(1))
+# print(lRUCache.get(3))
+# print(lRUCache.get(4))
