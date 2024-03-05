71f111562964dd3222f194b25ba9ca1165d2c6c8
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-03-05 17:43:13 +0100

    working on solution

--- sliding_window/76_minimum_window_substring.py
+++ sliding_window/76_minimum_window_substring.py
@@ -5,33 +5,60 @@ Output = "BANC"
 
 
 # Iterate through the string with the pointer until we find all the chars in t.
+# TO DO: Move left as much as you can without breaking the substring
+
 # The only way to make the substring shorter is to find a duplicate of the first char we found.
-# We save the relevant chars in a stack containing the chars value and their index and the count of each char in
-# our hmap with char values. 
-# When the pointer finds a duplicate of the char at the bottom of the stack we pop it from the stack and 
-# reduce it's value in the hashmap by 1. We continue trying to pop from the bottom of the stack by checking if
-# the value of the char at the bottom is > 1 in our hmap.
-# If not we calculate the difference between the index of the char we couldn't pop from the stack and our pointer.
-# We then compare that with the smallest substring we found so far. Continue until the value of the pointer is greater
-# than the length of the string.
+# Find the duplicate with the right pointer then move left as much as you can again.
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         l = 0
         r = 0
+        substr = {}
         chars = {}
         for char in t:
             chars[char] = 0
+            substr[char] = 1 + substr.get(char, 0)
+
         unique = len(t)
-        found = 0 
+        found = 0
+        startChar = ""
+        shortest = 0
+        finalstr = [0]
         while r < len(s):
-            if found < unique:
-                r += 1
-            else:
-                l += 1
-            if s[r] in chars:
-
-
-
+            char = s[r]
+            if char in chars:
+                if found < unique:
+                    if not found:
+                        startChar = char
+                    chars[char] += 1
+                    if chars[char] <= substr[char]:
+                        found += 1
+                        if found == unique:
+                            shortest = r + 1
+                            finalstr = [l, r]
+                            while chars[startChar] > substr[startChar]:
+                                l += 1
+                                lchar = s[l]
+                else:
+                    if char != startChar:
+                        chars[char] += 1
+                    else:
+                        while l < r:
+                            l += 1
+                            lchar = s[l]
+                            if lchar in chars:
+                                if chars[lchar] > substr[lchar]:
+                                    chars[lchar] -= 1
+                                else:
+                                    startChar = lchar
+                                    shortest = min(r - l + 1, shortest)
+                                    if r - l + 1 < shortest:
+                                        finalstr = [l, r]
+                                        shortest = r - l + 1
+                                    break
+            r += 1
+        result = s[finalstr[0]: finalstr[1]+1]
+        return result
 
 
 obj = Solution()
