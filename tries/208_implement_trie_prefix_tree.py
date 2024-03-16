606147904b6b08425b3a3332574f1873dcc6a84d
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-03-16 21:30:02 +0100

    solution with high runtime but low memory use

---
+++ tries/208_implement_trie_prefix_tree.py
@@ -0,0 +1,37 @@
+# Input
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+
+# Output
+[None, None, True, False, True, None, True]
+
+
+class Trie:
+
+    def __init__(self):
+        self.hmap = {}
+
+    def insert(self, word: str) -> None:
+        self.hmap[word] = 1
+
+    def search(self, word: str) -> bool:
+        if word in self.hmap:
+            return True
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        for key in self.hmap:
+            if key[0:len(prefix)] == prefix:
+                return True
+        return False
+
+
+test = {"asd": 5, "bsd": 3}
+asd = [key for key in test if key[0] == "a"]
+print(asd)
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
