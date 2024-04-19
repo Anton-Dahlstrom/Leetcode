29892cc45bb3bc39d1f4abad173455a6f1ed6b96
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-04-19 18:34:55 +0200

    added movedown function to heap class

--- heap_priorty_queue/1046_last_stone_weight.py
+++ heap_priorty_queue/1046_last_stone_weight.py
@@ -5,27 +5,40 @@ stones = [2, 7, 4, 1, 8, 1]
 Output: 1
 
 
-class MinHeap:
+class MaxHeap:
     def heapify(self, heap):
         for i in range(0, len(heap)):
             self.moveup(heap, i)
 
     def moveup(self, heap, child):
         parent = (child - 1)//2
-        while child > 0 and heap[parent] > heap[child]:
+        while child > 0 and heap[parent] < heap[child]:
             heap[parent], heap[child] = heap[child], heap[parent]
             child = parent
             parent = (child - 1)//2
 
+    def movedown(self, heap, parent):
+        child1 = parent * 2 + 1
+        child2 = parent * 2 + 2
+        while parent < len(heap) and child1 < len(heap) and (heap[parent] < heap[child1] or heap[parent] < heap[child2]):
+            if heap[child1] > heap[child2]:
+                child = child1
+            else:
+                child = child2
+            heap[parent], heap[child] = heap[child], heap[parent]
+            parent = child
+            child1 = parent * 2 + 1
+            child2 = parent * 2 + 2
 
-minheap = MinHeap()
-minheap.heapify(stones)
+        if child2 < len(heap):
+            if heap[parent] < heap[child2]:
+                heap[parent], heap[child] = heap[child], heap[parent]
 
 
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        heapq.heapify(stones)
-        print(stones)
+        heap = MaxHeap()
+        heap.heapify(stones)
 
 
 obj = Solution()
