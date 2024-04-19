01ecd9de99d1680b22e3043f8ae9757f994056fe
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-04-19 18:00:27 +0200

    added custom heapq class

--- heap_priorty_queue/1046_last_stone_weight.py
+++ heap_priorty_queue/1046_last_stone_weight.py
@@ -5,6 +5,23 @@ stones = [2, 7, 4, 1, 8, 1]
 Output: 1
 
 
+class MinHeap:
+    def heapify(self, heap):
+        for i in range(0, len(heap)):
+            self.moveup(heap, i)
+
+    def moveup(self, heap, child):
+        parent = (child - 1)//2
+        while child > 0 and heap[parent] > heap[child]:
+            heap[parent], heap[child] = heap[child], heap[parent]
+            child = parent
+            parent = (child - 1)//2
+
+
+minheap = MinHeap()
+minheap.heapify(stones)
+
+
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         heapq.heapify(stones)
