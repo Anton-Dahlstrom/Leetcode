51396840738701a3545a887b5575545c7d325bf9
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-05-05 23:33:12 +0200

    pseudocode for solution

--- heap_priorty_queue/621_task_scheduler.cs
+++ heap_priorty_queue/621_task_scheduler.cs
@@ -1,3 +1,7 @@
+// Count how many tasks each character needs.
+// Create sorted list and execute a task with the character with most tasks left that's available.
+// Use something to keep track of which chars are unavailable.
+// Count how many turns are used and return answer.
 public class Solution
 {
     public int LeastInterval(char[] tasks, int n)
@@ -13,6 +17,17 @@ public class Solution
             {
                 hmap.Add(key, 1);
             }
+        }
+        List<KeyValuePair<char, int>> stack = new List<KeyValuePair<char, int>>();
+        foreach (char key in hmap.Keys)
+        {
+            Console.WriteLine(key);
+            Console.WriteLine(hmap[key]);
+            stack.Add(new KeyValuePair<char, int>(key, hmap[key]));
+        }
+        foreach (KeyValuePair<char, int> kvp in stack)
+        {
+            Console.WriteLine($"{kvp.Key} {kvp.Value}");
         }
         return 0;
     }
