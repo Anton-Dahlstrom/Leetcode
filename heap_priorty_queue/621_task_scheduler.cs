ea00206b5e36ac3c6e9f360fe3f612ecef340f30
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-05-06 21:16:23 +0200

    working on solution

--- heap_priorty_queue/621_task_scheduler.cs
+++ heap_priorty_queue/621_task_scheduler.cs
@@ -2,6 +2,20 @@
 // Create sorted list and execute a task with the character with most tasks left that's available.
 // Use something to keep track of which chars are unavailable.
 // Count how many turns are used and return answer.
+
+public class ValueClass
+{
+    public char key;
+    public int val;
+    public int task;
+    public ValueClass(char letter, int value)
+    {
+        key = letter;
+        val = value;
+        task = int.MinValue;
+    }
+}
+
 public class Solution
 {
     public int LeastInterval(char[] tasks, int n)
@@ -18,17 +32,47 @@ public class Solution
                 hmap.Add(key, 1);
             }
         }
-        List<KeyValuePair<char, int>> stack = new List<KeyValuePair<char, int>>();
+        List<ValueClass> stack = new List<ValueClass>();
+
+        int i;
         foreach (char key in hmap.Keys)
         {
-            Console.WriteLine(key);
-            Console.WriteLine(hmap[key]);
-            stack.Add(new KeyValuePair<char, int>(key, hmap[key]));
+            for (i = 0; i < stack.Count(); i++)
+            {
+                if (hmap[key] > stack[i].val)
+                {
+                    break;
+                }
+            }
+            stack.Insert(i, new ValueClass(key, hmap[key]));
         }
-        foreach (KeyValuePair<char, int> kvp in stack)
+
+        int task = 0;
+        while (stack.Count > 0)
         {
-            Console.WriteLine($"{kvp.Key} {kvp.Value}");
+            task++;
+            for (i = 0; i < stack.Count; i++)
+            {
+                if (stack[i].task < task + n)
+                {
+                    stack[i].val -= 1;
+                    stack[i].task = task;
+                    for (i++; i < stack.Count; i++)
+                    {
+                        if (stack[i - 1].val < stack[i].val)
+                        {
+                            (stack[i], stack[i - 1]) = (stack[i - 1], stack[i]);
+                        }
+                        else { break; }
+                    }
+                }
+                if (stack.Count > 0 && stack[stack.Count - 1].val < 1)
+                {
+                    stack.RemoveAt(stack.Count - 1);
+                }
+            }
         }
-        return 0;
+        Console.WriteLine(task);
+        return task;
     }
 }
