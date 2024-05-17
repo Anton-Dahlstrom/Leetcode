83dabbf00a3d22c105163eea867b94c3d1a5ca8f
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-05-17 15:46:49 +0200

    working on solution

--- backtracking/79_word_search.cs
+++ backtracking/79_word_search.cs
@@ -1,38 +1,100 @@
+// TO DO:
+// Need to make it so the visited coordinates gets added to the correct char in the visited dict.
+
 public class Solution
 {
-    public IList<IList<int>> Exist(string[][] board, string word)
+    public bool Exist(char[][] board, string word)
     {
-        Dfs([.. nums], 0, 0, target);
-        foreach (IList<int> l in result)
+        Dictionary<char, IList<IList<int>>> visited = [];
+        Dfs(board, word, visited, 0, 0, 0);
+        for (int i = 0; i < board.Length; i++)
         {
-            foreach (int num in l)
+            for (int j = 0; j < board[i].Length; j++)
             {
-                Console.Write(num);
+                bool result = Dfs(board, word, visited, i, j, 0);
+                if (result)
+                {
+                    return true;
+                }
             }
-            Console.WriteLine("");
-            Console.WriteLine("--");
         }
-        return result;
+        return false;
     }
 
     // Need to make sure the GridSearch function doesn't try to visit a previous node.
     // Maybe by having a hashmap with each previously written letter as the key and a list
     // of coordinates as values, when a match is found it checks if the coordinates are
     // identical to any of the coordinate values in the hashmap and if so skips that coordinate.
-    public IList<IList<int>> GridSearch(char[][] board, int x, int y, int index, string target)
+    public IList<IList<int>> GridSearch(char[][] board, char target,
+            Dictionary<char, IList<IList<int>>> visited, int x, int y)
     {
+        Console.WriteLine("grid");
+        Console.WriteLine(target);
         IList<IList<int>> res = [];
+        bool adding = true;
         for (int i = Math.Max(y - 1, 0); i < Math.Min(board.Length, y + 1); i++)
         {
             for (int j = Math.Max(x - 1, 0); j < Math.Min(x + 1, board[i].Length); j++)
             {
-                if (board[i][j] == target[index])
+                if (board[i][j] == target)
+                    Console.WriteLine("FOUND");
                 {
-                    res.Add([i, j]);
+
+                    if (visited.TryGetValue(target, out IList<IList<int>>? value))
+                    {
+                        foreach (IList<int> arr in value)
+                        {
+                            if (arr[0] == i && arr[1] == j)
+                            {
+                                adding = false;
+                                break;
+                            }
+                        }
+                        if (adding)
+                        {
+                            res.Add([i, j]);
+                        }
+                        adding = true;
+                    }
+                    else
+                    {
+                        res.Add([i, j]);
+                    }
                 }
 
             }
         }
+        Console.WriteLine(res.Count);
         return res;
+    }
+
+    public bool Dfs(char[][] board, string word,
+            Dictionary<char, IList<IList<int>>> visited, int y, int x, int index)
+    {
+        Console.WriteLine("dfs");
+        if (index >= word.Length)
+        {
+            return true;
+        }
+        IList<IList<int>> coords = GridSearch(board, word[index], visited, y, x);
+        foreach (IList<int> coord in coords)
+        {
+            if (visited.TryGetValue(word[index], out IList<IList<int>>? value))
+            {
+                value.Add([y, x]);
+            }
+            else
+            {
+                IList<int> test = [y, x];
+                visited[word[index]] = [test];
+            }
+
+            bool res = Dfs(board, word, visited, coord[0], coord[1], index + 1);
+            if (res)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
