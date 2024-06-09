fd31e4ade782ff0b190c524cbc0fd51f8a1f837c
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-06-09 11:38:19 +0200

    attemped solution with simpler encoding

--- Trees/297_serialize_and_deserialize_binary_tree.py
+++ Trees/297_serialize_and_deserialize_binary_tree.py
@@ -15,16 +15,20 @@ class Codec:
         :rtype: str
         """
         if not root:
-            return
+            return ""
         self.res = f"{root.val}"
 
         def Dfs(node: TreeNode):
             if node.left:
                 self.res += f":{str(node.val)},l,{node.left.val}"
                 Dfs(node.left)
+            else:
+                self.res += ":#"
             if node.right:
                 self.res += f":{str(node.val)},r,{node.right.val}"
                 Dfs(node.right)
+            else:
+                self.res += ":#"
         Dfs(root)
         return self.res
 
@@ -39,32 +43,51 @@ class Codec:
         nodes = data.split(":")
         root = TreeNode(nodes[0])
         stack = [root]
-        for node in nodes[1:]:
-            values = node.split(",")
-            parent = values[0]
-            side = values[1]
-            child = TreeNode(values[2])
-            while stack:
-                cur = stack[-1]
-                if parent == cur.val:
-                    stack.append(child)
-                    if side == "l":
-                        cur.left = child
-                    else:
-                        cur.right = child
-                    break
-                else:
+        skip = False
+        print(data)
+        for i in range(1, len(nodes)):
+            print(i)
+            if skip:
+                skip = False
+                continue
+            side = "l"
+            cur = stack[-1]
+            if nodes[i][0] == "#":
+                if cur.left:
                     stack.pop()
+                    continue
+                if i == len(nodes)-1 or (i < len(nodes) - 1 and nodes[i+1][0] == "#"):
+                    skip = True
+                    stack.pop()
+                    continue
+                else:
+                    i += 1
+                    skip = True
+                    side = "r"
+            values = nodes[i].split(",")
+            child = TreeNode(values[2])
+            if side == "r" or cur.left:
+                cur.right = child
+            else:
+                cur.left = child
+            stack.append(child)
+
         return root
 
 
 # Your Codec object will be instantiated and called as such:
-root = TreeNode(3)
-root.left = TreeNode(2)
-root.right = TreeNode(4)
-root.left.left = TreeNode(3)
-print(root.left.left.val)
+# root = TreeNode(3)
+# root.left = TreeNode(2)
+# root.right = TreeNode(4)
+# root.left.left = TreeNode(3)
+# print(root.left.left.val)
+root = TreeNode(1)
+root.right = TreeNode(2)
 ser = Codec()
 deser = Codec()
 ans = deser.deserialize(ser.serialize(root))
-print(ans.left.left.val)
+# print("-----")
+print(ans.val)
+# print(ans.left.val)
+print(ans.right.val)
+# print(ans.left.left.val)
