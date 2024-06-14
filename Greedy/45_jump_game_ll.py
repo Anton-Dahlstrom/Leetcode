8a44ca87fd012218cbe47ef94245110f9949074f
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-06-14 22:15:43 +0200

    WIP

--- Greedy/45_jump_game_ll.py
+++ Greedy/45_jump_game_ll.py
@@ -15,26 +15,41 @@ Output: 1
 nums = [1, 2, 1, 1, 1]
 Output: 3
 
+# nums = [0]
+
+# nums = [1, 2]
+
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        i = 0
-        steps = 1
-        while True:
-            val = nums[i]
-            if i + val >= len(nums)-1:
-                break
-            longest = 0
-            for j in range(i+1, i+1+val):
-                reach = nums[j] + (j - i)
-                if i == 1:
-                    print(i+1, i+1+val)
-                    print(i, j, val, longest, reach)
-                if reach > longest:
-                    i = j
-                    longest = reach
-            steps += 1
-        return steps
+        if len(nums) < 2:
+            return 0
+        # The index we are jumping from
+        cur = 0
+        # Determines how far we can search
+        reach = nums[0]
+        # Keeps track of how far we searched last jump so we can start
+        # from the index we stopped at before.
+        jumps = 0
+        while cur < len(nums):
+            print(cur, len(nums))
+            jumps += 1
+            tempreach = reach
+            for i in range(1, reach+1):
+                if cur + i >= len(nums)-1:
+                    print('hi')
+                    return jumps
+                # The index we are looking through will be further up in the list
+                # and therefore reach further than our previous node.
+
+                tempreach -= 1
+                if nums[cur+i] + reach >= tempreach:
+                    # This is wrong
+                    tempreach = nums[cur+1] + reach
+            cur = cur + reach
+            reach = tempreach
+            print(cur)
+        return jumps
 
 
 obj = Solution()
