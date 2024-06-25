a601bc7461373ce1c83ded6cdc13365532331f02
Anton <anton.dahlstrom@hotmail.com>
2024-06-25 21:52:54 +0200

    WIP

--- intervals/435_non-overlapping_intervals.py
+++ intervals/435_non-overlapping_intervals.py
@@ -6,8 +6,8 @@ output = 1
 intervals = [[-25322, -4602], [-35630, -28832], [-33802, 29009], [13393, 24550], [-10655, 16361], [-2835, 10053], [-2290, 17156], [1236, 14847], [-45022, -1296], [-34574, -1993], [-14129, 15626], [3010, 14502],
              [42403, 45946], [-22117, 13380], [7337, 33635], [-38153, 27794], [47640, 49108], [40578, 46264], [-38497, -13790], [-7530, 4977], [-29009, 43543], [-49069, 32526], [21409, 43622], [-28569, 16493], [-28301, 34058]]
 output = 19
-
-
+# intervals.sort()
+# print(intervals)
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         def checkOverlap(interval1, interval2):
@@ -34,13 +34,18 @@ class Solution:
 
         key = max(hmap, key=hmap.get)
         while hmap[key][0] > 0:
+            # if count == 16:
+            #     break
             count += 1
             val = hmap.pop(key)
             for interval in val[1]:
                 hmap[interval][0] -= 1
                 hmap[interval][1].remove(key)
             key = max(hmap, key=hmap.get)
-
+        for key in hmap:
+            print(key)
+            print(hmap[key])
+            print("----------")
         return count
 
 
