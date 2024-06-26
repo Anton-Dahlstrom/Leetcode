5d00657cf980af7b86cebb9a02da23c9ba215f59
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-06-26 22:28:42 +0200

    wip

--- backtracking/131_palindrome_partitioning.py
+++ backtracking/131_palindrome_partitioning.py
@@ -1,39 +1,43 @@
 from typing import List
 
+s = "aab"
+output = [["a", "a", "b"], ["aa", "b"]]
+
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        res = []
-        temp = []
+        self.res = []
+        self.temp = []
+        self.dfs(s, 0)
+        return self.res
 
-        def dfs(s: str, index):
-            for i in range(index+1, len(s)+1):
-                print(index, i)
-                print(s[index: i])
-                if checkPalindrome(s[index:i]):
-                    temp.append(s[index:i])
-                    if i == len(s):
-                        print("APPENDING: ", temp)
-                        res.append(temp)
-                    else:
-                        dfs(s, i+1)
-                    temp.pop()
-
-        def checkPalindrome(s: str):
-            l = len(s)//2
-            r = len(s)//2
-            if not len(s) % 2:
-                l -= 1
-            while l >= 0:
-                if s[l] == s[r]:
-                    l -= 1
-                    r += 1
+    def dfs(self, s: str, index):
+        for i in range(index, len(s)+1):
+            print(index, i)
+            print(s[index: i])
+            if self.checkPalindrome(s[index:i+1]):
+                self.temp.append(s[index:i+1])
+                print(s[index:i+1])
+                if i == len(s):
+                    print("APPENDING: ", self.temp)
+                    self.res.append(self.temp.copy())
+                    print(self.res)
                 else:
-                    return False
-            return True
+                    self.dfs(s, i+1)
+                self.temp.pop()
 
-        dfs(s, 0)
-        return res
+    def checkPalindrome(self, s: str):
+        l = len(s)//2
+        r = len(s)//2
+        if not len(s) % 2:
+            l -= 1
+        while l >= 0:
+            if s[l] == s[r]:
+                l -= 1
+                r += 1
+            else:
+                return False
+        return True
 
 
 s = "aab"
