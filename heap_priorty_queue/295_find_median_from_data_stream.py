367f652b1460f321546d243c68c929e85e677fdc
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-07-01 23:45:29 +0200

    1000ms 8% runtime, 37mb 90% memory

--- heap_priorty_queue/295_find_median_from_data_stream.py
+++ heap_priorty_queue/295_find_median_from_data_stream.py
@@ -1,66 +1,45 @@
-import math
 class MedianFinder:
 
     def __init__(self):
-        self.heap = []
+        self.nums = []
 
     def addNum(self, num: int) -> None:
-        self.heap.append(num)
-        i = len(self.heap)-1
-        while i > 0:
-            parent = (i - 1)//2 
-            if self.heap[parent] < self.heap[i]:
-                temp = self.heap[i]
-                self.heap[i] = self.heap[parent]
-                self.heap[parent] = temp
-            i = parent
-        print(self.heap)
-#            0
-#     1,             2
-# 3,     4,      5,      6
-#7, 8, 9, 10, 11, 12, 13, 14
-# 4-7 == 3
-#
-# 1, 3, 7, 15
+        if not self.nums or num > self.nums[-1]:
+            self.nums.append(num)
+            return
+        l = 0
+        r = len(self.nums)
+        while l <= r:
+            mid = l + ((r-l)//2)
+            if num == self.nums[mid]:
+                self.nums.insert(mid, num)
+                return
+            elif num < self.nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        if num > self.nums[mid]:
+            self.nums.insert(max(mid+1, 0), num)
+            return
+        self.nums.insert(mid, num)
 
-    def findMedian(self) -> float:
-        odd = len(self.heap) % 2
-        cuts = math.log2((len(self.heap) //2))
-        sets = [(self.heap[0], 0)]
-        count = 0
-        while count < cuts:
-            count += 1
-            i = 0
-            for i in range(len(sets)):
-                if sets[i][1] < len(self.heap) //2:
-                    break
-            set = sets[i]
-            i = set[1]
-            l = i*2 + 1
-            if l < len(self.heap):
-                l = (self.heap[l], l)
-                j = 0
-                while l[0] > self.heap[j] and j < len(self.heap):
-                    j += 1
-                sets.insert(j, l)
-            r = i*2 + 2
-            if r < len(self.heap):
-                r = (self.heap[r], r)
-                j = 0
-                while r[0] > self.heap[j] and j < len(self.heap):
-                    j += 1
-                sets.insert(j, r)
-        print(sets)
-        if odd:
-            return float(sets[0][0])
-        return (sets[0][0] + sets[1][0]) / 2
+    def findMedian(self):
+        length = len(self.nums)
+        if length % 2:
+            return self.nums[length//2]
+        return (self.nums[length//2] + self.nums[length//2-1])/2
 
-# Your MedianFinder object will be instantiated and called as such:
+
+arr = [[6], [], [10], [], [2], [], [6], [], [5], [], [
+    0], [], [6], [], [3], [], [1], [], [0], [], [0], []]
+output = [6, 8.0, 6, 6.0, 6, 5.5, 6, 5.5, 5, 4.0, 3]
+
 obj = MedianFinder()
-# obj.addNum(2)
-obj.addNum(4)
-obj.addNum(5)
-# obj.addNum(7)
-
-res = obj.findMedian()
+res = []
+for val in arr:
+    if not val:
+        res.append(obj.findMedian())
+    else:
+        obj.addNum(val[0])
 print(res)
+print(res == output)
