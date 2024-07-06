5d2f41cad43b39237d875381851c9045ff1ca509
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-07-06 13:46:08 +0200

    36ms 56% runtime, 16mb 25% memory

--- 1-D_dynamic_programming/91_decode_ways.py
+++ 1-D_dynamic_programming/91_decode_ways.py
@@ -1,26 +1,48 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        self.res = 0
+        if not s or s[0] == "0":
+            return 0
+        res = 1
+        fib1 = 0
+        fib2 = 1
         x = 64
         charRange = range(65, 91)
+        for i in range(0, len(s)):
+            if s[i] == "0":
+                if i < len(s)-1:
+                    if s[i+1] == "0":
+                        return 0
+                res *= fib1 + fib2
+                fib1, fib2 = 0, 1
+                continue
+            if i < len(s)-1:
+                if s[i+1] == "0":
+                    if (int(s[i]) * 10 + int(s[i+1])) + x not in charRange:
+                        return 0
+                    res *= fib1 + fib2
+                    fib1, fib2 = 0, 1
+                    continue
+            if i > 0:
+                if s[i-1] == "0":
+                    continue
+                combined = (int(s[i-1]) * 10 + int(s[i])) + x
+                if combined in charRange:
+                    fib1, fib2 = fib2, fib1+fib2
+                else:
+                    res *= fib1 + fib2
+                    fib1, fib2 = 0, 1
+        res *= fib1 + fib2
+        return res
 
-        def dfs(s):
-            if not s:
-                self.res += 1
-                return
-            if s[0] == "0":
-                return
-            dfs(s[1:])
-            if len(s) > 1 and (int(s[0]) * 10) + int(s[1]) + x in charRange:
-                dfs(s[2:])
-        dfs(s)
-        return self.res
 
+s = "111111111111111111111111111111111111111111111"
+output = 1836311903
 
-s = "226"
-output = 3
 s = "27"
 output = 1
+
+s = "230"
+output = 0
 
 obj = Solution()
 res = obj.numDecodings(s)
