42edae22abcb012542da343f411b8f330208f093
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-07-10 11:44:48 +0200

    83ms 10% runtime, 17mb 15% memory

--- 1-D_dynamic_programming/152_maximum_product_subarray.py
+++ 1-D_dynamic_programming/152_maximum_product_subarray.py
@@ -6,53 +6,47 @@ class Solution:
         def maxOrNone(values):
             candidates = [val for val in values if val is not None]
             return max(candidates) if candidates else None
-        res = negative = prevPositive = curPositive = None
+
+        res = positive = negative = None
+        tempPos = None
         for val in nums:
+            print(res, positive, negative)
             if val == 0:
-                res = maxOrNone([0, res, negative, prevPositive, curPositive])
-                negative = prevPositive = curPositive = None
+                res = maxOrNone(
+                    [0, res, positive, negative])
+                positive = negative = None
                 continue
-            elif val < 0:
+            res = maxOrNone(
+                [res, positive, negative])
+
+            if val < 0:
+                if negative:
+                    tempPos = negative * val
+                if positive:
+                    negative = positive * val
+                else:
+                    negative = val
+                positive = tempPos
+                tempPos = None
+            else:
+                if positive:
+                    positive *= val
+                else:
+                    positive = val
                 if negative:
                     negative *= val
-                    if prevPositive:
-                        negative *= prevPositive
-                    if curPositive:
-                        curPositive *= negative
-                    else:
-                        curPositive = negative
-                    res = maxOrNone([res, negative, prevPositive, curPositive])
-                    negative = None
-                else:
-                    prevPositive = curPositive
-                    curPositive = None
-                    negative = val
-                continue
-            if not curPositive:
-                curPositive = val
-            else:
-                curPositive *= val
-        res = maxOrNone([res, negative, prevPositive, curPositive])
+
+        print(res, positive, negative)
+        res = maxOrNone([res, positive, negative])
         return res
 
 
-nums = [2, 3, -2, 4]
-output = 6
-
-nums = [-2]
-output = -2
-
-nums = [-3, -1, -1]
-output = 3
-
-nums = [-2, 0, -1]
-output = 0
-
-nums = [-2, -3, 7]
-output = 42
+# nums = [-2, 0, -1]
+# output = 0
 
 nums = [2, -5, -2, -4, 3]
 output = 24
+
 
 obj = Solution()
 res = obj.maxProduct(nums)
