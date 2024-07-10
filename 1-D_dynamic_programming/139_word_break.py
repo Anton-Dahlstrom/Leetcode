d965a36cd8c7d671a8592b39b9052a0870790539
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-07-10 12:59:00 +0200

    30ms 96% runtime, 16mb 78% memory

--- 1-D_dynamic_programming/139_word_break.py
+++ 1-D_dynamic_programming/139_word_break.py
@@ -3,70 +3,38 @@ from typing import List
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        if s and not wordDict:
-            return False
-        temp = []
-        print(wordDict, s)
-        wordEnding = False
+        if not s:
+            return True
+        temp = [word for word in wordDict if word[0] == s[0]]
         for char in s:
-            tempEnding = False
-            found = False
+            ending = False
+            if not temp:
+                return False
             for i in reversed(range(0, len(temp))):
                 if temp[i][0] == char:
-                    found = True
                     if temp[i][1:]:
                         temp[i] = temp[i][1:]
                     else:
-                        tempEnding = True
+                        temp.pop(i)
+                        ending = True
                 else:
-                    print("True")
-                    tempEnding = True
                     temp.pop(i)
-            if tempEnding or wordEnding or not temp:
-                wordEnding = False
-                for word in wordDict:
-                    if word[0] == char:
-                        found = True
-                        if word[1:]:
-                            temp.append(word[1:])
-                        else:
-                            wordEnding = True
-            if not found:
-                return False
-            print(char, temp)
-        print(tempEnding, wordEnding)
-        if tempEnding or wordEnding:
+            if ending:
+                temp += wordDict
+        if ending:
             return True
         return False
 
-
-s = "leetcode"
-wordDict = ["leet", "code"]
-output = True
 
 s = "catsandog"
 wordDict = ["cats", "dog", "sand", "and", "cat"]
 output = False
 
-# s = "a"
-# wordDict = ["a"]
-# output = True
-
-# s = "a"
-# wordDict = ["b"]
-
-# s = "abcd"
-# wordDict = ["a", "abc", "b", "cd"]
-# output = True
-
-# s = "aaaaaaa"
-# wordDict = ["aaaa", "aa"]
+s = "aaaaaaa"
+wordDict = ["aaaa", "aa"]
+output = False
 
 obj = Solution()
 res = obj.wordBreak(s, wordDict)
 print(res)
-
-# asd = []
-# a = "abcd"
-# asd.append(a[4:])
-# print(asd)
+print(res == output)
