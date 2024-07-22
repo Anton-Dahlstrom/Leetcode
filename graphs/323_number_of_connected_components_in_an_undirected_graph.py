dc5075455250796b9538a450948397bd68d26db4
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-07-22 20:32:28 +0200

    neetcode solution

--- graphs/323_number_of_connected_components_in_an_undirected_graph.py
+++ graphs/323_number_of_connected_components_in_an_undirected_graph.py
@@ -1,11 +1,11 @@
 class Solution:
     def countComponents(self, n: int, edges: list[list[int]]) -> int:
+        if not edges:
+            return n
         edgehmap = {}
         for edge in edges:
-            if edge[1] in edgehmap:
-                edge[1].append(edge[0])
-            else:
-                edgehmap.setdefault(edge[0], []).append(edge[1])
+            edgehmap.setdefault(edge[0], []).append(edge[1])
+            edgehmap.setdefault(edge[1], []).append(edge[0])
 
         def dfs(val):
             res = False
@@ -13,21 +13,25 @@ class Solution:
                 visited.add(val)
             if val in edgehmap:
                 for v in edgehmap[val]:
-                    if dfs(v):
-                        res = True
+                    if v not in visited:
+                        if dfs(v):
+                            res = True
             return res
 
+        count = 0
         visited = set()
-        for node in edgehmap:
-            if node not in visited:
-                dfs(node)
-        print(edgehmap)
-        pass
+        for i in range(n):
+            if i not in visited:
+                if not dfs(i):
+                    count += 1
+
+        return count
 
 
 n = 6
 edges = [[0, 1], [1, 2], [2, 3], [4, 5]]
 output = 2
+
 
 obj = Solution()
 res = obj.countComponents(n, edges)
