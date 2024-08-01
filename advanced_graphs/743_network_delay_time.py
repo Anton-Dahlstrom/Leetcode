ce6b8bad479e447d63abb7be4e81e84fdb51be4c
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-08-01 18:56:50 +0200

    3388ms 5% runtime, 19mb 7% memory

--- advanced_graphs/743_network_delay_time.py
+++ advanced_graphs/743_network_delay_time.py
@@ -12,49 +12,26 @@ class Solution:
         if k not in source:
             return -1
 
-        visited = set()
-        res = [0]*(n+1)
-        print(res)
-        print(source)
-        # Visited prevents optimal path from being found.
+        shortest = {}
 
         def dfs(node, cur):
-            print(res)
-            for key in source[node]:
-                temp = cur + source[node][key]
-                if key not in visited:
-                    visited.add(key)
-                    if key in source:
-                        dfs(key, temp)
-                if not res[key]:
-                    res[key] = temp
-                else:
-                    res[key] = min(res[key], temp)
+            if node in source:
+                for edge in source[node]:
+                    temp = cur + source[node][edge]
+                    if edge not in shortest:
+                        shortest[edge] = temp
+                        dfs(edge, temp)
+                    elif temp < shortest[edge]:
+                        shortest[edge] = temp
+                        dfs(edge, temp)
 
-        visited.add(k)
+        shortest[k] = 0
         dfs(k, 0)
-        print(res)
-        print(visited)
-        if len(visited) < n:
-            return -1
-        res[k] = 0
-        return max(res)
 
+        if len(shortest) == n:
+            return max(shortest.values())
+        return -1
 
-times = [[2, 1, 1], [2, 3, 1], [3, 4, 1]]
-n = 4
-k = 2
-output = 2
-
-# times = [[1, 2, 1], [2, 3, 2], [1, 3, 2]]
-# n = 3
-# k = 1
-# output = 2
-
-times = [[1, 2, 1], [2, 1, 3]]
-n = 2
-k = 2
-output = 3
 
 times = [[3, 5, 78], [2, 1, 1], [1, 3, 0], [4, 3, 59], [5, 3, 85], [5, 2, 22], [2, 4, 23], [1, 4, 43], [4, 5, 75], [5, 1, 15], [
     1, 5, 91], [4, 1, 16], [3, 2, 98], [3, 4, 22], [5, 4, 31], [1, 2, 0], [2, 5, 4], [4, 2, 51], [3, 1, 36], [2, 3, 59]]
