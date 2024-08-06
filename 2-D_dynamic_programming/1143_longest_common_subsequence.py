e38b134135995c53c8ea1d4afc9c2410045cf30d
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-08-06 21:00:06 +0200

    WIP

--- 2-D_dynamic_programming/1143_longest_common_subsequence.py
+++ 2-D_dynamic_programming/1143_longest_common_subsequence.py
@@ -1,26 +1,34 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         set1 = set(text1)
-        both = set()
+        common_chars = set1.intersection(text2)
+        text1 = "".join(char for char in text1 if char in common_chars)
+        text2 = "".join(char for char in text2 if char in common_chars)
+        print(text1)
+        print(text2)
         hmap1 = {}
         hmap2 = {}
-        for char in text2:
-            if char in set1:
-                both.add(char)
-        text1 = "".join([char for char in text1 if char in both])
-        text2 = "".join([char for char in text2 if char in both])
         for i, char in enumerate(text1):
             hmap1.setdefault(char, []).append(i)
         for i, char in enumerate(text2):
             hmap2.setdefault(char, []).append(i)
 
+        # yqpqaar
+        # yraqp
+        # r = 1, start = 6, 1
+        # a = 2, start = 5, 2
+        # a = 2, start = 4, 2
+        # q = 1, start = 3, 3
+        # p = 1, start = 2, 4
+        # q = 2, start = 1, 3
+        # y = 3, start = 0, 0
         print(hmap1)
         print(hmap2)
 
 
 text1 = "ylqpejqbalahwr"
 text2 = "yrkzavgdmdgtqpg"
-
+# yqp
 output = 3
 
 obj = Solution()
