4e0bb45710540ea123c47f096b727834c7751461
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-08-10 23:24:47 +0200

    wip

--- 2-D_dynamic_programming/309_best_time_to_buy_and_sell_stock_with_cooldown.py
+++ 2-D_dynamic_programming/309_best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -1,23 +1,27 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        res = 0
         memo = [0]*len(prices)
 
-        def dfs(i):
-            pass
         for i in reversed(range(0, len(prices)-1)):
             price = prices[i]
             if price < prices[i+1]:
                 memo[i] = prices[i+1] - price
+        if len(prices) < 4:
+            return max(prices) - min(prices)
 
-        print(memo)
-        print(prices)
-        # [1,2,3,0,2]
-        # [1,1,0,2,0]
+        for i in range(3, len(memo)):
+            memo[i] = max(memo[i-4], memo[i-3]) + memo[i]
+        return max(memo)
 
 
 prices = [1, 2, 3, 0, 2]
 output = 3
+
+prices = [1, 2, 4]
+output = 3
+
+prices = [2, 1]
+output = 0
 
 obj = Solution()
 res = obj.maxProfit(prices)
