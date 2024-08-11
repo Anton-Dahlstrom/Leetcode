fcfcb81b98605c4d355f5de2fc45882a1979d973
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-08-11 23:53:53 +0200

    WIP

--- 2-D_dynamic_programming/309_best_time_to_buy_and_sell_stock_with_cooldown.py
+++ 2-D_dynamic_programming/309_best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -6,22 +6,39 @@ class Solution:
             price = prices[i]
             if price < prices[i+1]:
                 memo[i] = prices[i+1] - price
-        if len(prices) < 4:
-            return max(prices) - min(prices)
 
+        print(prices)
+        print(memo)
+        if len(memo) < 4:
+            return sum(memo)
         for i in range(3, len(memo)):
-            memo[i] = max(memo[i-4], memo[i-3]) + memo[i]
+            print(i, prices[i] - min(prices[:i]), max(memo[i-4], memo[i-3],
+                                                      memo[i-3] + memo[i-4]) + memo[i])
+            memo[i] = max(prices[i] - min(prices[:i]), max(memo[i-4], memo[i-3],
+                                                           memo[i-3] + memo[i-4]) + memo[i])
+        print(memo)
         return max(memo)
 
 
-prices = [1, 2, 3, 0, 2]
+prices = [2, 1, 2, 1, 0, 1, 2]
 output = 3
 
-prices = [1, 2, 4]
-output = 3
+# [6,6,5,5,3,0]
+# prices = [6, 1, 3, 2, 4, 7]
+# output = 6
 
-prices = [2, 1]
-output = 0
+# prices = [1, 2, 3, 0, 2]
+# output = 3
+
+# prices = [1, 2, 4]
+# output = 3
+
+# prices = [2, 1]
+# output = 0
+
+# prices = [1, 2, 4, 4, 4, 4]
+# output = 3
+
 
 obj = Solution()
 res = obj.maxProfit(prices)
