96946ae985cb2f3158c1b3427908471fbdaf5069
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-08-13 23:57:48 +0200

    wip

--- 2-D_dynamic_programming/309_best_time_to_buy_and_sell_stock_with_cooldown.py
+++ 2-D_dynamic_programming/309_best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -1,5 +1,18 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
+        # lowest ignoring jumps [1, 1, 1, -2, -2]
+        # lowest factoring jumps [1, 1, 2, -1, -1]
+        # [1-1=0, 2-1=1, 3-1=2, 0-1=-1, 2--1=3]
+        # [1-0 = 1,
+        # 2-1 = 1: 2-1 = 1,
+        # 3-1 = 2: 3-2 = 1,
+        # 0-, -4]
+        # prices = [1, 2, 3, 0, 2]
+        # output = 3
+        test = [0]*len(prices)
+        for i in range(0, len(prices)):
+            start = max(0, i-4)
+            price = prices[i]
 
         memo = [0]*len(prices)
 
@@ -43,8 +56,10 @@ output = 6
 
 # lowest ignoring jumps [1, 1, 1, -2, -2]
 # lowest factoring jumps [1, 1, 2, -1, -1]
-# prices = [1, 2, 3, 0, 2]
-# output = 3
+# [1-1=0, 2-1=1, 3-1=2, 0-1=-1, 2--1=3]
+# [0, -1, -2, -2, -4]
+prices = [1, 2, 3, 0, 2]
+output = 3
 
 # prices = [1, 2, 4]
 # output = 3
