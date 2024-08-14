b3f79825e1d8207f108a771724836ade06e031c7
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-08-14 23:07:00 +0200

    working on solution

--- 2-D_dynamic_programming/309_best_time_to_buy_and_sell_stock_with_cooldown.py
+++ 2-D_dynamic_programming/309_best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -1,65 +1,62 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        # lowest ignoring jumps [1, 1, 1, -2, -2]
-        # lowest factoring jumps [1, 1, 2, -1, -1]
-        # [1-1=0, 2-1=1, 3-1=2, 0-1=-1, 2--1=3]
-        # [1-0 = 1,
-        # 2-1 = 1: 2-1 = 1,
-        # 3-1 = 2: 3-2 = 1,
-        # 0-, -4]
-        # prices = [1, 2, 3, 0, 2]
-        # output = 3
-        test = [0]*len(prices)
-        for i in range(0, len(prices)):
-            start = max(0, i-4)
-            price = prices[i]
-
-        memo = [0]*len(prices)
-
-        # How much money can be made by buying and instantly selling.
-        for i in reversed(range(0, len(prices)-1)):
-            price = prices[i]
-            # Should save past profits to "reduce" price of buying to find total profit.
-            # Should also do all calculations inside this loop.
-            if price < prices[i+1]:
-                memo[i] = prices[i+1] - price
-
+        res = 0
+        trades = [0]*len(prices)
+        lowestPrice = prices[0]
         print(prices)
-        print(memo)
-        if len(memo) < 4:
-            return sum(memo)
-        for i in range(3, len(memo)):
-            print(i, prices[i] - min(prices[:i]), max(memo[i-4], memo[i-3],
-                                                      memo[i-3] + memo[i-4]) + memo[i])
-            # difference in lowest price to current price:
-            arg1 = prices[i] - min(prices[:i])
-            # best profit we have made in the past plus best profit we can make at this index.
-            arg2 = max(memo[i-4], memo[i-3], memo[i-3] + memo[i-4]) + memo[i]
-            # best profit in the past plus largest difference in recent price
-            arg3 = prices[i] - min(prices[i-2:i]) + max(memo[:i-2])
-            memo[i] = max(arg1, arg2, arg3)
+        # Need to update price with best trade as it becomes available
+        # When i = 6 we update prices[3] by adding the best trade
+        # [0, 0, -2, -2, -2, -5, -6]
+        # prices when i = 5: [3, 3, 5, 0, 0, 3, 1, 4]
+        # prices when i = 6: [3, 3, 5, -2, 0, 3, 1, 4]
+        # We then update and look at lowestPrice
+        # prices = [3, 3, 5, 0, 0, 3, 1, 4]
+        # output = 6
+        for i in range(0, len(prices)):
+            if i > 3:
+                first = prices[i-1] + min(trades[i-4:i-2])
+                second = min(prices[i-2:i]) + trades[i-4]
+                best = min(first, second)
+                prices[i-4] = trades[i-4]
+                lowestPrice = min(lowestPrice, prices[i-3])
+            elif i > 2:
+                best = min(0, prices[i-1] + trades[i-3])
+            else:
+                best = min(prices[:i+1])
+            lowestPrice = min(lowestPrice, prices[i])
+            trade = min(best, lowestPrice) - prices[i]
+            res = min(res, trade)
+            trades[i] = min(trade, res)
+            print(res)
+        print(prices)
+        print(trades)
+        return abs(res)
 
-            # memo[i] = max(prices[i] - min(prices[:i]), max(memo[i-4], memo[i-3],
-            #                                                memo[i-3] + memo[i-4]) + memo[i],
-            #               prices[i] - min(prices[i-2:i]) + max(memo[:i-2]))
-        print(memo)
-        return max(memo)
 
-
-prices = [2, 1, 2, 1, 0, 1, 2]
+prices = [1, 4, 2]
 output = 3
+
+prices = [3, 3, 5, 0, 0, 3, 1, 4]
+output = 6
+
+# prices = [6, 1, 3, 2, 4, 7]
+# output = 6
+
+# prices = [2, 1, 2, 1, 0, 1, 2]
+# output = 3
 
 # backwards = [6, 6, 5, 5, 3, 0]
 # lowest [6, 1, 1, 1, 1, 1]
-prices = [6, 1, 3, 2, 4, 7]
-output = 6
+# prices = [6, 1, 3, 2, 4, 7]
+# output = 6
 
 # lowest ignoring jumps [1, 1, 1, -2, -2]
 # lowest factoring jumps [1, 1, 2, -1, -1]
 # [1-1=0, 2-1=1, 3-1=2, 0-1=-1, 2--1=3]
 # [0, -1, -2, -2, -4]
-prices = [1, 2, 3, 0, 2]
-output = 3
+
+# prices = [1, 2, 3, 0, 2]
+# output = 3
 
 # prices = [1, 2, 4]
 # output = 3
