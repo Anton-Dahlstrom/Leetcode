db1f9c687d4b49c7b20276f52b928a9f8fd64b42
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-08-18 23:46:37 +0200

    working on new approach

--- 2-D_dynamic_programming/518_ coin_change_ii.py
+++ 2-D_dynamic_programming/518_ coin_change_ii.py
@@ -1,20 +1,56 @@
+import heapq
+import copy
+
+
 class Solution:
     def change(self, amount: int, coins: list[int]) -> int:
-        res = 0
-        stack = [amount]
-        while stack:
-            print(stack)
-            cur = stack.pop()
-            for c in coins:
-                temp = cur - c
-                if temp == 0:
-                    print("adding", cur, c)
-                    res += 1
-                elif temp > 0:
-                    stack.append(temp)
-        print(stack)
-        print(res)
-        return
+        coins.sort()
+        self.res = 0
+        hmap = {}
+        for i, c in enumerate(coins):
+            hmap[c] = [[0]*len(coins)]
+            hmap[c][0][i] += 1
+            # counter[c] = 1
+
+        heap = coins.copy()
+        heapq.heapify(heap)
+
+        while heap:
+            cur = heapq.heappop(heap)
+            combination = hmap.pop(cur)
+            if cur == amount:
+                return len(combination)
+            for i, c in enumerate(coins):
+                val = cur + c
+                if val <= amount:
+                    curComb = combination.copy()
+                    if val in hmap:
+                        for comb in curComb:
+                            comb[i] += 1
+                            if comb not in hmap[val]:
+                                hmap[val].append(comb.copy())
+                    else:
+                        hmap[val] = []
+                        for comb in curComb:
+                            comb[i] += 1
+                            hmap[val].append(comb.copy())
+                        heapq.heappush(heap, val)
+
+        return -1
+
+        # self.res = 0
+        # def dfs(i, cur):
+        #     print(cur)
+        #     if cur == 0:
+        #         self.res += 1
+        #         return
+        #     for j in range(i, len(coins)):
+        #         val = cur - coins[j]
+        #         if val < 0:
+        #             return
+        #         dfs(j, val)
+        # dfs(0, amount)
+        # return self.res
 
 
 amount = 20
