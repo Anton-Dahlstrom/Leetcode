df71af582dcd1ca3ba03226d0da07931c0e3ac3e
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-08-19 23:07:44 +0200

    slower hash solution still failing

--- 2-D_dynamic_programming/518_ coin_change_ii.py
+++ 2-D_dynamic_programming/518_ coin_change_ii.py
@@ -1,56 +1,46 @@
-import heapq
 import copy
+import heapq
 
 
 class Solution:
     def change(self, amount: int, coins: list[int]) -> int:
-        coins.sort()
-        self.res = 0
-        hmap = {}
-        for i, c in enumerate(coins):
-            hmap[c] = [[0]*len(coins)]
-            hmap[c][0][i] += 1
-            # counter[c] = 1
 
-        heap = coins.copy()
-        heapq.heapify(heap)
-
+        heap = [0]
+        base = ".".join([str(c-c) for c in coins])
+        hmap = {0: {base}}
         while heap:
+            asd = 6
             cur = heapq.heappop(heap)
-            combination = hmap.pop(cur)
+            combinations = hmap.pop(cur)
+            print(cur)
+            # print(combinations)
             if cur == amount:
-                return len(combination)
+                return len(combinations)
             for i, c in enumerate(coins):
                 val = cur + c
                 if val <= amount:
-                    curComb = combination.copy()
-                    if val in hmap:
-                        for comb in curComb:
-                            comb[i] += 1
-                            if comb not in hmap[val]:
-                                hmap[val].append(comb.copy())
-                    else:
-                        hmap[val] = []
-                        for comb in curComb:
-                            comb[i] += 1
-                            hmap[val].append(comb.copy())
+                    if val not in hmap:
+                        temp = set()
+                        for comb in combinations:
+                            combArr = comb.split(".")
+                            newVal = int(combArr[i]) + 1
+                            combArr[i] = str(newVal)
+                            # combArr[i] += str(int(combArr[i]) + 1)
+                            combHash = ".".join([str(v) for v in combArr])
+                            temp.add(combHash)
+                        hmap[val] = temp
                         heapq.heappush(heap, val)
+                    else:
+                        for comb in combinations:
+                            combArr = comb.split(".")
+                            newVal = int(combArr[i]) + 1
+                            combArr[i] = str(newVal)
+                            # combArr[i] += str(int(combArr[i]) + 1)
+                            combHash = ".".join([str(v) for v in combArr])
+                            if combHash not in hmap[val]:
+                                hmap[val].add(combHash)
 
-        return -1
-
-        # self.res = 0
-        # def dfs(i, cur):
-        #     print(cur)
-        #     if cur == 0:
-        #         self.res += 1
-        #         return
-        #     for j in range(i, len(coins)):
-        #         val = cur - coins[j]
-        #         if val < 0:
-        #             return
-        #         dfs(j, val)
-        # dfs(0, amount)
-        # return self.res
+        return 0
 
 
 amount = 20
@@ -61,6 +51,13 @@ amount = 5
 coins = [1, 2, 5]
 output = 4
 
+amount = 500
+coins = [1, 2, 5]
+output = 12701
+
+# amount = 100
+# coins = [3, 5, 7, 8, 9, 10, 11]
+# output = 6606
 
 obj = Solution()
 res = obj.change(amount, coins)
