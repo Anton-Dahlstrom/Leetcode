38175f35e3420ac56e7e52a67cc401fe962d764c
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-08-22 16:52:15 +0200

    brute force solution that exceeds time limit

---
+++ 2-D_dynamic_programming/97_interleaving_string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+
+        def dfs(p1, p2):
+            p3 = p1 + p2
+            if p3 == len(s3):
+                return True
+            if p1 < len(s1) and s1[p1] == s3[p3]:
+                if dfs(p1+1, p2):
+                    return True
+            if p2 < len(s2) and s2[p2] == s3[p3]:
+                if dfs(p1, p2+1):
+                    return True
+            return False
+        return dfs(0, 0)
+
+
+s1 = "aabcc"
+s2 = "dbbca"
+s3 = "aadbbcbcac"
+output = True
+
+obj = Solution()
+res = obj.isInterleave(s1, s2, s3)
+print(res)
+print(res == output)
