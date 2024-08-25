78297ecb367a699bba9a78d0e2e5de72db1bd414
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-08-25 22:31:15 +0200

    trying to add cache

--- 2-D_dynamic_programming/312_burst_balloons.py
+++ 2-D_dynamic_programming/312_burst_balloons.py
@@ -1,20 +1,37 @@
 class Solution:
     def maxCoins(self, nums: list[int]) -> int:
         nums = [1] + nums + [1]
+        visited = {}
+        indexes = [i for i in range(len(nums))]
+        self.test = 0
 
-        def dfs(arr):
+        def dfs(indexArr, removed):
+            # if removed in visited:
+            #     return visited[removed]
             best = 0
-            for i in range(1, len(arr)-1):
-                temp = arr.copy()
-                val = arr[i-1] * arr[i] * arr[i+1]
-                temp.pop(i)
-                best = max(best, val + dfs(temp))
+            for i in range(1, len(indexArr)-1):
+                tempIndexArr = indexArr.copy()
+                tempRemoved = removed[:i] + "1" + removed[i+1:]
+                val = nums[indexArr[i-1]] * \
+                    nums[indexArr[i]] * nums[indexArr[i+1]]
+                tempIndexArr.pop(i)
+                best = max(best, val + dfs(tempIndexArr, tempRemoved))
+            if removed in visited:
+                print(best, visited[removed])
+            visited[removed] = best
+            print(visited)
             return best
-        return dfs(nums)
+
+        res = dfs(indexes, "0"*(len(nums)-2))
+        print(visited)
+        return res
 
 
 nums = [3, 1, 5, 8]
 output = 167
+
+nums = [3, 1, 5]
+output = 35
 
 obj = Solution()
 res = obj.maxCoins(nums)
