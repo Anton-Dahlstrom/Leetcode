d995bd69a236885e0173dfbb84a5c46057b88338
Anton <anton.dahlstrom@hotmail.com>
2024-09-04 20:00:10 +0200

    40ms 58% runtime, 16mb 50% memory

---
+++ math_and_geometry/43_multiply_strings.py
@@ -0,0 +1,26 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        int1 = 0
+        int2 = 0
+        for i, char in enumerate(num1):
+            val = (ord(char) - 48) * (10 ** (len(num1) - 1 - i))
+            int1 += val
+        for i, char in enumerate(num2):
+            val = (ord(char) - 48) * (10 ** (len(num2) - 1 - i))
+            int2 += val
+        print(int1, int2)
+        return str(int1 * int2)
+
+num1 = "123"
+num2 = "456"
+output = "56088"
+
+num1 = "9"
+num2 = "99"
+output = "891"
+
+print(output)
+obj = Solution()
+res = obj.multiply(num1, num2)
+print(res)
+print(res == output)
