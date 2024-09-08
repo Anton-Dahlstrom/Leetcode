636615350f2f85b7003a1f1e839f87193fb58330
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-09-08 15:05:12 +0200

    6200ms 14% runtime, 16mb 74% memory

--- 2-D_dynamic_programming/10_regular_expression_matching.py
+++ 2-D_dynamic_programming/10_regular_expression_matching.py
@@ -1,54 +1,50 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
 
-        # Need to account for * allowing the previous character to not be matched.
-
         def dfs(i, j):
             if i == len(s) and j == len(p):
                 return True
 
-            if i == len(s) or j == len(p):
+            if j < (len(p)-1) and p[j+1] == "*":
+                if p[j] == ".":
+                    while i < len(s):
+                        if dfs(i, j+2):
+                            return True
+                        i += 1
+                    return dfs(i, j + 2)
+
+                elif i < len(s) and s[i] == p[j]:
+                    char = p[j]
+                    if dfs(i, j + 2):
+                        return True
+                    i += 1
+                    while i < len(s) and s[i] == char:
+                        if dfs(i, j + 2):
+                            return True
+                        i += 1
+                    return dfs(i, j + 2)
+
+                else:
+                    return dfs(i, j+2)
+
+            # Can't advance i because there is no more pattern to use.
+            if j == len(p):
+                return False
+            # Can't skip current pattern and move j forward because next is not "*".
+            if i == len(s):
                 return False
 
-            if p[j] == "*":
-
-                if p[j-1] == "." or s[i] == p[j-1]:
-                    if dfs(i+1, j):
-                        return True
-                    return dfs(i+1, j+1)
-                else:
-                    return dfs(i, j+1)
-
-            if p[j] == ".":
+            elif p[j] == "." or s[i] == p[j]:
                 return dfs(i+1, j+1)
 
-            elif p[j] == s[i]:
-                return dfs(i+1, j+1)
-
-            return dfs(i, j+1)
+            return False
 
         return dfs(0, 0)
 
 
-# s = "aa"
-# p = "a*"
-# output = True
-
-# s = "aa"
-# p = "a"
-# output = False
-
-# s = "aab"
-# p = "c*a*b"
-# output = True
-
-# s = "ab"
-# p = ".*c"
-# output = False
-
-s = "aaa"
-p = "ab*a*c*a"
-output = True
+s = "mississippi"
+p = "mis*is*p*."
+output = False
 
 obj = Solution()
 res = obj.isMatch(s, p)
