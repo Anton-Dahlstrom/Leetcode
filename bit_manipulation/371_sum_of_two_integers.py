f29c1d0abf21c2e9cc782b7e3a4cdc975e8b5432
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-09-12 21:42:59 +0200

    added functions for finding bits

--- bit_manipulation/371_sum_of_two_integers.py
+++ bit_manipulation/371_sum_of_two_integers.py
@@ -13,21 +13,63 @@ class Solution:
             return oldKeep
 
         def removeSmallNegative(large, small):
+            small = abs(small)
+            # First we found how far we have to shift to find the bit we need to remove:
+
+            # Returns how many times you need to shift to find the first bit.
+            def findShiftsToFirstBit(num):
+                compare = num
+                shift = num
+                count = 1
+                while True:
+                    shift >>= 1
+                    shift <<= 1
+                    count <<= 1
+                    if shift == compare and shift > 0:
+                        shift >>= 1
+                        compare >>= 1
+                        continue
+                    break
+                count >>= 1
+                return count
+
+            # Returns all the bits in a given amount of shifts.
+            def findBitsInInterval(num, count):
+                compare = num
+                shift = num
+                base = 1
+                res = 1
+                for _ in range(count):
+                    shift >>= 1
+                    shift <<= 1
+                    cur <<= 1
+                    if shift != compare:
+                        res = res | base
+                        pass
+                    shift >>= 1
+                    compare >>= 1
+                    break
+                res <<= 1
+                print(bin(res))
+                return res
+
             # Keep whatever was removed from large before we find the first bit
             # in small.
-            # If we remove 4 from 8 we keep everything below 8 except 2 and 1
+            # If we remove 4 from 8 we keep everything below 8 besides except
+            # for the values below the value we removed (in this case 4 so 2 and 1).
             # 8 - 4 = 1000 - 0100 = 0100
             # 8 = 2 = 1000 - 0010 = 0110
             # 16-4 = 10000 - 00100 = 01100
-            small = abs(small)
-# test
+
+        # If a is negative and smaller than B.
         if (a < 0 and 0 < b):
             if abs(a) < b:
-                removeSmallNegative(b, a)
+                return removeSmallNegative(b, a)
 
-        if (b < 0 and 0 < a):
+        # If b is negative and msaller than B
+        elif (b < 0 and 0 < a):
             if abs(b) < a:
-                removeSmallNegative(a, b)
+                return removeSmallNegative(a, b)
         return addTwoPositive(a, b)
 
 
