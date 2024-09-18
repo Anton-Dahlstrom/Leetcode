858fee484723d2b42e4b0a4d4bb124893a553e97
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-09-18 13:37:32 +0200

    225ms 85% runtime, 18mb 27% memory

--- math_and_geometry/2013_detect_squares.py
+++ math_and_geometry/2013_detect_squares.py
@@ -24,36 +24,26 @@ class DetectSquares:
 
     def count(self, point: list[int]) -> int:
         x, y = point[0], point[1]
-        # print(x, y)
-        # print(self.x)
-        # print(self.y)
         if x not in self.x or y not in self.y:
             return 0
         res = 0
-        xcopy = self.x.copy()
-        ycopy = self.y.copy()
         for ykey in self.x[x]:
             for xkey in self.y[y]:
-                if xkey in self.y[ykey] and abs(y-ykey) == abs(x-xkey):
-                    print(x, y, xkey, ykey)
-                    print(f"[[{x}, {y}], [{x}, {ykey}], [{xkey}, {y}], [{xkey}, {ykey}], ")
+                if xkey in self.y[ykey] and abs(y-ykey) == abs(x-xkey) and abs(y-ykey) > 0:
                     temp = 1
                     temp *= self.x[x][ykey]
                     temp *= self.y[y][xkey]
                     temp *= self.y[ykey][xkey]
                     res += temp
-        self.x = xcopy
-        self.y = ycopy
         return res
 
-commands = ["DetectSquares","add","add","add","count","count","add","count"]
-input = [[],[[3,10]],[[11,2]],[[3,2]],[[11,10]],[[14,8]],[[11,2]],[[11,10]]] 
 
-# commands = ["DetectSquares","add","add","add","count"]
-# input = [[],[[3,10]],[[11,2]],[[3,2]],[[11,10]]] 
-
-commands = ["DetectSquares","add","add","add","count","add","add","add","count","add","add","add","count","add","add","add","count"]
-input = [[],[[5,10]],[[10,5]],[[10,10]],[[5,5]],[[3,0]],[[8,0]],[[8,5]],[[3,5]],[[9,0]],[[9,8]],[[1,8]],[[1,0]],[[0,0]],[[8,0]],[[8,8]],[[0,8]]]
+commands = ["DetectSquares", "add", "add",
+            "add", "count", "count", "add", "count"]
+input = [[], [[3, 10]], [[11, 2]], [[3, 2]], [
+    [11, 10]], [[14, 8]], [[11, 2]], [[11, 10]]]
+output = [1, 0, 2]
+res = []
 
 # Your DetectSquares object will be instantiated and called as such:
 obj = DetectSquares()
@@ -61,4 +51,8 @@ for i in range(1, len(commands)):
     if commands[i] == "add":
         obj.add(input[i][0])
     else:
-        print(obj.count(input[i][0]))
+        count = obj.count(input[i][0])
+        res.append(count)
+
+print(res)
+print(output == res)
