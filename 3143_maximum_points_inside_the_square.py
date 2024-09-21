96258b5cd0d16e5f6d72112de4a12bbc478e60e9
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-09-21 23:53:07 +0200

    wip

--- 3143_maximum_points_inside_the_square.py
+++ 3143_maximum_points_inside_the_square.py
@@ -1,8 +1,8 @@
-from typing import List
-
-
 class Solution:
-    def maxPointsInsideSquare(self, points: List[List[int]], s: str) -> int:
+    def maxPointsInsideSquare(self, points: list[list[int]], s: str) -> int:
+        # After the largest size the square can be is found (the cap)
+        # we need to loop through all the characters and see which fit.
+        # For each that fit we add 1 to our result.
         hmap = {}
         size = 0
         cap = float('inf')
@@ -11,15 +11,15 @@ class Solution:
             l = max(abs(points[i][0]), abs(points[i][1]))
             if char in hmap:
                 if hmap[char][1] < s:
-                    cap = s
+                    cap = min(cap, s, hmap[char][1])
                 elif hmap[char][0] > l:
-                    cap = hmap[char][0]
-                    hmap[char] = [s, l]
-
+                    cap = min(cap, hmap[char][0])
+                    hmap[char][0] = [l]
             else:
                 hmap[char] = [s, l]
                 size = max(size, l)
-
+        print(hmap)
+        print(size, cap)
         return min(size, cap)
 
 
@@ -27,9 +27,9 @@ points = [[2, 2], [-1, -2], [-4, 4], [-3, 1], [3, -3]]
 s = "abdca"
 # 2
 
-# points = [[1, 1], [-1, -1], [2, -2]]
-# s = "ccd"
-# # 0
+points = [[1, 1], [-1, -1], [2, -2]]
+s = "ccd"
+# 0
 
 
 sol = Solution()
