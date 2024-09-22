c25ad802cdabe88f9c268edb17ff13c40e09eb66
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-09-22 20:24:52 +0200

    930ms 46% runtime, 60mb 49% memory

--- 3143_maximum_points_inside_the_square.py
+++ 3143_maximum_points_inside_the_square.py
@@ -1,37 +1,27 @@
 class Solution:
     def maxPointsInsideSquare(self, points: list[list[int]], s: str) -> int:
-        # After the largest size the square can be is found (the cap)
-        # we need to loop through all the characters and see which fit.
-        # For each that fit we add 1 to our result.
         hmap = {}
-        size = 0
         cap = float('inf')
+        res = 0
         for i, char in enumerate(s):
-            s = min(abs(points[i][0]), abs(points[i][1]))
-            l = max(abs(points[i][0]), abs(points[i][1]))
+            size = max(abs(points[i][0]), abs(points[i][1]))
             if char in hmap:
-                if hmap[char][1] < s:
-                    cap = min(cap, s, hmap[char][1])
-                elif hmap[char][0] > l:
-                    cap = min(cap, hmap[char][0])
-                    hmap[char][0] = [l]
+                cap = min(cap, max(hmap[char], size))
+                hmap[char] = min(hmap[char], size)
             else:
-                hmap[char] = [s, l]
-                size = max(size, l)
-        print(hmap)
-        print(size, cap)
-        return min(size, cap)
+                hmap[char] = size
+        for char in hmap:
+            if hmap[char] < cap:
+                res += 1
+        return res
 
 
 points = [[2, 2], [-1, -2], [-4, 4], [-3, 1], [3, -3]]
 s = "abdca"
-# 2
-
-points = [[1, 1], [-1, -1], [2, -2]]
-s = "ccd"
-# 0
+output = 2
 
 
 sol = Solution()
 res = sol.maxPointsInsideSquare(points, s)
 print(res)
+print(res == output)
