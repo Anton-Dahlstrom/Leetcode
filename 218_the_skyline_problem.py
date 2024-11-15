5890d5eae73327d71ced6747d025114bc1cf668f
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-11-15 23:49:30 +0100

    wip ranges

--- 218_the_skyline_problem.py
+++ 218_the_skyline_problem.py
@@ -2,28 +2,32 @@ class Solution:
     def getSkyline(self, buildings: list[list[int]]) -> list[list[int]]:
         if not buildings:
             return []
-        heights = []
+        ranges = [[0, float("inf"), 0]]
         for building in buildings:
             left, right, height = building
-            if right > len(heights):
-                heights += [0]*(right - len(heights) + 1)
-            for i in range(left, right):
-                heights[i] = max(heights[i], height)
-
-        res = []
-        prevHeight = 0
-        for i in range(0, len(heights)):
-            if heights[i] != prevHeight:
-                prevHeight = heights[i]
-                temp = [i, heights[i]]
-                res.append(temp)
-        return res
+            i = 0
+            while i < len(ranges):
+                temp = []
+                rleft, rright, rheight = ranges[i]
+                if left > rright:
+                    continue
+                if right < rleft:
+                    break
+                if height > rheight:
+                    if left > rleft:
+                        temp.append([rleft, left-1, rheight])
+                    temp.append([left, right, height])
+                    if right < rright:
+                        temp.append([right+1, rright, rheight])
+                ranges = ranges[:i] + temp + ranges[i+1:]
+                i += len(temp)
+        print(ranges)
 
 
 buildings = [[0, 2, 3], [2, 5, 3]]
 output = [[0, 3], [5, 0]]
 
-buildings = [[2, 9, 10], [3, 7, 15], [5, 12, 12], [15, 20, 10], [19, 24, 8]]
+# buildings = [[2, 9, 10], [3, 7, 15], [5, 12, 12], [15, 20, 10], [19, 24, 8]]
 
 obj = Solution()
 res = obj.getSkyline(buildings)
