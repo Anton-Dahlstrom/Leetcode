1d9635d1d4b6360007c4dbac2d5727dbc0e60024
Anton <anton.dahlstrom@hotmail.com>
2024-11-29 13:55:09 +0100

    1180ms 42% runtime, 44mb 16% memory

--- 2577_minimum_time_to_visit_a_cell_in_a_grid.py
+++ 2577_minimum_time_to_visit_a_cell_in_a_grid.py
@@ -1,24 +1,41 @@
+import heapq
 class Solution:
     def minimumTime(self, grid: list[list[int]]) -> int:
-        coords = {(0,0)}
-        time = 0
-        while coords:
-            time += 1
-            temp = set()
-            while coords:
-                row, col = coords.pop()
-                if row == len(grid) - 1 and col == len(grid[0])-1:
-                    return time -1
-                for nrow in range(row-1, row+2, 2):
-                    if nrow in range(0,len(grid)):
-                        if grid[nrow][col] <= time:
-                            temp.add((nrow, col))
-                for ncol in range(col-1, col+2, 2):
-                    if ncol in range(0, len(grid[0])):
-                        if grid[row][ncol] <= time:
-                            temp.add((row, ncol))
-            coords = temp
-        return -1
+        if (grid[1][0] > 1 and grid[0][1] > 1) or grid[0][0] > 0:
+            return -1
+        heap = [(1,(0,0))]
+        heapq.heapify(heap)
+        visited = {(0,0)}
+        while heap:
+            time, coords = heapq.heappop(heap)
+            row, col = coords
+            if row == len(grid)-1 and col == len(grid[0])-1:
+                return time -1
+            for nrow in range(row-1, row+2, 2):
+                if nrow in range(0,len(grid)) and (nrow, col) not in visited:
+                    visited.add((nrow, col))
+                    if grid[nrow][col] > time:
+                        newtime = grid[nrow][col]
+                        timeDiff = grid[nrow][col] - time
+                        if timeDiff % 2:
+                            newtime += 1
+                        newtime += 1
+                    else:
+                        newtime = time + 1
+                    heapq.heappush(heap, (newtime, (nrow, col)))
+                        
+            for ncol in range(col-1, col+2, 2):
+                if ncol in range(0,len(grid[0])) and (row, ncol) not in visited:
+                    visited.add((row, ncol))
+                    if grid[row][ncol] > time:
+                        newtime = grid[row][ncol]
+                        timeDiff = grid[row][ncol] - time
+                        if timeDiff % 2:
+                            newtime += 1
+                        newtime += 1
+                    else:
+                        newtime = time + 1
+                    heapq.heappush(heap, (newtime, (row, ncol)))
 
 grid = [[0,1,3,2],
         [5,1,2,5],
@@ -27,6 +44,6 @@ output = 7
 
 obj = Solution()
 res = obj.minimumTime(grid)
-print(output)
 print(res)
+print(output)
 print(res == output)
