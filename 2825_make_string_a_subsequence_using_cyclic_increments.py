eae4462d57ee85c98bfdc5f9c46d14a82cafd4c4
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-12-04 10:59:00 +0100

    155ms 6% runtime, 42mb 5% memory

---
+++ 2825_make_string_a_subsequence_using_cyclic_increments.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        # ord: a = 97, z = 122
+
+        def dfs(str1, str2, i1, i2):
+            if i2 == len(str2):
+                return True
+            if i1 == len(str1):
+                return False
+
+            ord1, ord2 = ord(str1[i1]), ord(str2[i2])
+            if ord1 == ord2 or ord1+1 == ord2 or (ord1 == 122 and ord2 == 97):
+                if dfs(str1, str2, i1+1, i2+1):
+                    return True
+            else:
+                return dfs(str1, str2, i1+1, i2)
+            return False
+
+        return dfs(str1, str2, 0, 0)
+
+
+str1 = "abc"
+str2 = "ad"
+output = True
+
+obj = Solution()
+res = obj.canMakeSubsequence(str1, str2)
+print(res)
+print(output)
+print(res == output)
