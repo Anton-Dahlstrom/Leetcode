0d326f39b3918681865edaebf2e8bf72d4d9549f
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-12-09 18:14:06 +0100

    46ms 58% runtime, 46mb 53% memory

--- 3152_special_array_ii.py
+++ 3152_special_array_ii.py
@@ -1,30 +1,32 @@
 class Solution:
     def isArraySpecial(self, nums: list[int], queries: list[list[int]]) -> list[bool]:
-        # Need to find areas of array that are "unsafe" ahead of time.
         res = []
+        breakpoints = []
+        for i in range(len(nums)-1):
+            if (nums[i] % 2 + nums[i+1] % 2) != 1:
+                breakpoints.append((i))
+        breakpoints.append(len(nums))
+        arr = [0]*len(nums)
+        j = 0
+        i = 0
+        while i < len(nums):
+            if i > breakpoints[j]:
+                j += 1
+            arr[i] = breakpoints[j]
+            i += 1
+
         for query in queries:
             start, stop = query
-
-            i = start
-            while i < stop:
-                print(nums[i] % 2, nums[i+1] % 2)
-                if (nums[i] % 2 + nums[i+1] % 2) != 1:
-                    res.append(False)
-                    break
-                i += 1
-            if i == stop:
+            if arr[start] < stop:
+                res.append(False)
+            else:
                 res.append(True)
-
         return res
 
 
 nums = [4, 3, 1, 6]
 queries = [[0, 2], [2, 3]]
 output = [False, True]
-
-# nums = [2, 1]
-# queries = [[0, 1]]
-# output = [True]
 
 obj = Solution()
 res = obj.isArraySpecial(nums, queries)
