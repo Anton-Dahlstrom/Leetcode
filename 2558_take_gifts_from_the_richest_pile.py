4d8a816c2ecb48b8c1d975c65edbfb8f224f7159
Anton <anton.dahlstrom@hotmail.com>
2024-12-12 14:43:17 +0100

    3ms 87% runtime, 17mb 8% memory

---
+++ 2558_take_gifts_from_the_richest_pile.py
@@ -0,0 +1,24 @@
+import heapq
+import math
+class Solution:
+    def pickGifts(self, gifts: list[int], k: int) -> int:
+        heap = [] 
+        for gift in gifts:
+            heapq.heappush(heap, gift*-1)
+        print(heap)
+        for _ in range(k):
+            val = heapq.heappop(heap)
+            val = int(math.sqrt(val*-1))
+            heapq.heappush(heap, val*-1)
+        res = sum(heap) *-1
+        return res
+
+gifts = [25,64,9,4,100]
+k = 4
+output= 29
+
+obj = Solution()
+res = obj.pickGifts(gifts, k)
+print(res)
+print(output)
+print(res == output)
