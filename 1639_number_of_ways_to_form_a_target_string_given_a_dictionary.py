fbff43f87420d478e4c2d9df349598bce782cfaf
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-12-29 17:56:34 +0100

    8500ms 5% runtime, 90mb 34% memory

--- 1639_number_of_ways_to_form_a_target_string_given_a_dictionary.py
+++ 1639_number_of_ways_to_form_a_target_string_given_a_dictionary.py
@@ -1,29 +1,19 @@
 class Solution:
     def numWays(self, words: list[str], target: str) -> int:
-        visited = set()
-
-        def dfs(state, pos, maxi, used):
-            if pos == len(target):
-                return 1
-            usedtuple = tuple(used)
-            if usedtuple in visited:
-                return 0
-            visited.add(usedtuple)
-            res = 0
-            for i in range(len(words)):
-                start = state[i]
-                for j in range(state[i], len(words[i])):
-                    if target[pos] == words[i][j]:
-                        state[i] = j+1
-                        if j > maxi:
-                            used.append((i, j))
-                            res += dfs(state, pos+1, j, used)
-                            used.pop()
-                state[i] = start
-            return res
-
-        state = [0] * len(words)
-        return dfs(state, 0, -1, []) % (1000000000 + 7)
+        arr = [[0 for _ in range(len(words[0]))] for _ in range(len(target))]
+        diff = len(words[0]) - len(target)
+        for i in range(len(arr)):
+            for j in range(i, i + diff + 1):
+                for word in words:
+                    if word[j] == target[i]:
+                        arr[i][j] += 1
+        for i in range(len(arr)-1):
+            temp = [0] * len(words[0])
+            for j in range(i, i + diff + 1):
+                for k in range(j+1, i+diff+2):
+                    temp[k] += arr[i][j] * arr[i+1][k]
+            arr[i+1] = temp
+        return sum(arr[-1]) % (1000000000+7)
 
 
 words = ["acca", "bbbb", "caca"]
