73d98eacbbd68b7bd8d5634bf17b6d182177495d
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-01-12 20:08:07 +0100

    80ms 79% runtime, 18mb 57% memory

---
+++ 2116_check_if_a_parentheses_can_be_valid.py
@@ -0,0 +1,39 @@
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        if len(s) % 2 or (s[0] == ")" and locked[0] == "1") or (s[-1] == "(" and locked[-1] == "1"):
+            return False
+        space = 0
+        for i in range(len(s)):
+            if s[i] == ")" and locked[i] == "1":
+                if not space:
+                    return False
+                space -= 1
+            else:
+                space += 1
+        space = 0
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == "(" and locked[i] == "1":
+                if not space:
+                    return False
+                space -= 1
+            else:
+                space += 1
+        return True
+
+
+s = "))()))"
+locked = "010100"
+output = True
+
+# s = "()()))()())(((()((()((()((()()()))(()()((()((()()(((()())))))()((()(()(())((()()((())))))))(())(())))()()()((()())())(()()))((((((())()())())))))())((((()())(())(())()()()(()(()((()))((((()((()((()())(())((((())(())))(()((((())))((()(((((()()))))((((()))))())()))))())"
+# locked = "()()))()())(((()((()((()((()()()))(()()((()((()()(((()())))))()((()(()(())((()()((())))))))(())(())))()()()((()())())(()()))((((((())()())())))))())((((()())(())(())()()()(()(()((()))((((()((()((()())(())((((())(())))(()((((())))((()(((((()()))))((((()))))())()))))())"
+
+s = "()"
+locked = "11"
+output = True
+
+obj = Solution()
+res = obj.canBeValid(s, locked)
+print(res)
+print(output)
+print(res == output)
