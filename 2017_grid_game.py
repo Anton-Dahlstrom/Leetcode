0801508baf874436519c97cab23407d7f246795b
Anton <anton.dahlstrom@hotmail.com>
2025-01-21 23:33:35 +0100

    140ms 18% runtime, 30mb 36% memory

---
+++ 2017_grid_game.py
@@ -0,0 +1,31 @@
+class Solution:
+    def gridGame(self, grid: list[list[int]]) -> int:
+        if len(grid[0]) == 1:
+            return 0
+
+        for i in range(len(grid[0])-2,0,-1):
+            grid[0][i] += grid[0][i+1]
+        for i in range(1,len(grid[0])):
+            grid[1][i] += grid[1][i-1]
+
+        grid[0][0] = grid[0][1]
+
+        for g in grid:
+            print(g)
+
+        best = min(grid[0][1], grid[1][-2])
+        for i in range(1, len(grid[0])-1):
+            best = min(best, max(grid[1][i-1], grid[0][i+1]))
+        return best
+
+
+grid = [[1,3,1,15],[1,3,3,1]]
+output = 7
+
+
+obj = Solution()
+res = obj.gridGame(grid)
+print(res)
+print(output)
+print(res == output)
+        
