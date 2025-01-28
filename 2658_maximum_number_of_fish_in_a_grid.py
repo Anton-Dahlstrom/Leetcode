488b1ed5fc00be99615ae1eb8ac02ada2a254301
Anton <anton.dahlstrom@hotmail.com>
2025-01-28 09:05:00 +0100

    95ms 6% runtime, 18mb 49% memory

--- 2658_maximum_number_of_fish_in_a_grid.py
+++ 2658_maximum_number_of_fish_in_a_grid.py
@@ -1,24 +1,29 @@
 class Solution:
     def findMaxFish(self, grid: list[list[int]]) -> int:
         res = 0
-        visited = set()
+        directions = [(1,0), (-1, 0), (0,1), (0,-1)]
 
-        def dfs(pos):
-            visited.add(pos)
-            r, c = pos
-            total = grid[r][c]
-            for nr in range(r-1, r+2, 2):
-                if nr in range(0, len(grid)) and (nr, c) not in visited and grid[nr][c] > 0:
-                    total += dfs((nr, c))
-            for nc in range(c-1, c+2, 2):
-                if nc in range(0, len(grid[0])) and (r, nc) not in visited and grid[r][nc] > 0:
-                    total += dfs((r, nc))
+        def isValid(r, c):
+            if r in range(len(grid)) and c in range(len(grid[0])):
+                return True
+            return False
+
+        def dfs(r,c): 
+            if not grid[r][c]:
+                return 0
+            total = 0
+            total += grid[r][c]
+            grid[r][c] = 0
+            for nrow, ncol in directions:
+                nrow += r
+                ncol += c
+                if isValid(nrow, ncol) and grid[nrow][ncol]:
+                    total += dfs(nrow, ncol)
             return total
 
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if grid[r][c] > 0 and (r, c) not in visited:
-                    res = max(res, dfs((r, c)))
+                res = max(dfs(r,c), res)
 
         return res
 
