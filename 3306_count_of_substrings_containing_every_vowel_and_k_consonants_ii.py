d25a61c7d1e64097fca971c6c0ff1fd02b3c5039
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-03-10 22:47:49 +0100

    wip

---
+++ 3306_count_of_substrings_containing_every_vowel_and_k_consonants_ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+        res = 0
+        n = len(word)
+
+        l = 0
+        r = 0
+        while r < n:
+            pass
+
+        return res
+
+
+word = "aeiou"
+k = 0
+output = 1
+
+obj = Solution()
+res = obj.countOfSubstrings(word, k)
+print(output)
+print(res)
+print(res == output)
