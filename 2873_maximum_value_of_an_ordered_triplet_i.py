f72ab0744429a041e44f0a5563d4102b520fc31f
Anton <anton.dahlstrom@hotmail.com>
2025-04-02 12:57:10 +0900

    4ms 70% runtime, 18mb 19% memory

--- 2873_maximum_value_of_an_ordered_triplet_i.py
+++ 2873_maximum_value_of_an_ordered_triplet_i.py
@@ -2,10 +2,17 @@ class Solution:
     def maximumTripletValue(self, nums: list[int]) -> int:
         n = len(nums)
         res = 0
-        for i in range(n):
-            for j in range(i+1, n):
-                for k in range(j+1, n):
-                    res = max(res, (nums[i] - nums[j]) * nums[k])
+        prefix = [0]*n
+        curmax = 0
+        for i in range(n-1, -1, -1):
+            curmax = max(curmax, nums[i])
+            prefix[i] = curmax
+        # keep track of biggest val left
+        # prefix for biggest val right
+        lmax = nums[0]
+        for i in range(1, n-1):
+            res = max(res, (lmax - nums[i]) * prefix[i+1])
+            lmax = max(lmax, nums[i])
         return res
 
 
