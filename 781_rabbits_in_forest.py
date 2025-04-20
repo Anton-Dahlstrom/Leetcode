ad3d6c2f6aa6ca62ea88b1deff81bb5bdbf90cc6
Anton <anton.dahlstrom@hotmail.com>
2025-04-21 06:36:52 +0900

    3ms 28% runtime, 18mb 51% memory

---
+++ 781_rabbits_in_forest.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+
+class Solution:
+    def numRabbits(self, answers: list[int]) -> int:
+        hmap = defaultdict(int)
+        res = 0
+        for ans in answers:
+            hmap[ans] += 1
+            if hmap[ans] == 1:
+                res += ans+1
+            if hmap[ans] > ans:
+                hmap[ans] = 0
+        return res
+
+
+answers = [1, 1, 2]
+output = 5
+
+answers = [1, 0, 1, 0, 0]
+output = 5
+
+obj = Solution()
+res = obj.numRabbits(answers)
+print(res)
+print(output)
+print(res == output)
