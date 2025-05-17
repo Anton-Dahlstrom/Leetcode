033ac91d3815e53aa6e6c7537b8a4b62f02ab786
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-05-17 16:26:00 +0200

    0ms 100% runtime, 17mb 53% memory

--- 75_sort_colors.py
+++ 75_sort_colors.py
@@ -1,31 +1,26 @@
 class Solution:
     def sortColors(self, nums: list[int]) -> None:
+        n = len(nums)
+        zeros, ones = 0, 0
+        for i in range(n):
+            if nums[i] == 0:
+                zeros += 1
+            elif nums[i] == 1:
+                ones += 1
 
-        def quickSort(arr, start, pivot):
-            startPivot = pivot
-            i = start
-            while i < pivot:
-                if arr[i] > arr[pivot]:
-                    if pivot - i > 1:
-                        arr[pivot], arr[pivot-1] = arr[pivot-1], arr[pivot]
-                    arr[i], arr[pivot] = arr[pivot], arr[i]
-                    pivot -= 1
-                else:
-                    i += 1
-            if (pivot - start) > 1:
-                quickSort(arr, start, pivot-1)
-            if (startPivot - pivot) > 1:
-                quickSort(arr, pivot+1, startPivot)
-
-        quickSort(nums, 0, len(nums)-1)
+        for i in range(n):
+            if zeros:
+                nums[i] = 0
+                zeros -= 1
+            elif ones:
+                nums[i] = 1
+                ones -= 1
+            else:
+                nums[i] = 2
 
 
 nums = [2, 0, 2, 1, 1, 0]
 output = [0, 0, 1, 1, 2, 2]
-
-nums = [1, 0]
-output = [0, 1]
-
 
 obj = Solution()
 res = obj.sortColors(nums)
