bcc90bc8cb7c17008e347b44ae8b3a86aec1d416
Anton <anton.dahlstrom@hotmail.com>
2026-01-31 20:24:53 +0100

    0ms 100% runtime, 48mb 40% memory

---
+++ 744_find_smallest_letter_greater_than_target.cs
@@ -0,0 +1,12 @@
+public class Solution
+{
+    public char NextGreatestLetter(char[] letters, char target)
+    {
+        foreach (var c in letters)
+        {
+            if (c > target)
+                return c;
+        }
+        return letters[0];
+    }
+}
