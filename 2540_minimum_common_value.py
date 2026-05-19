442ad28f13605c380ceb787fdb932b51f43a1e0c
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2026-05-19 21:55:48 +0200

    7ms 85% runtime, 42mb 13% memory

---
+++ 2540_minimum_common_value.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        nums1 = set(nums1)
+        for num in nums2:
+            if num in nums1:
+                return num
+        return -1
