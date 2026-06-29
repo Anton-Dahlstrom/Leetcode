6d0037389be100ab224b2b3014459fe3c1740b35
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2026-06-29 19:57:44 +0200

    0ms 100% runtime, 19mb 6% memory

---
+++ 1967_number_of_strings_that_appear_as_substrings_in_word.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        res = 0
+        for p in patterns:
+            if p in word:
+                res += 1
+        return res
